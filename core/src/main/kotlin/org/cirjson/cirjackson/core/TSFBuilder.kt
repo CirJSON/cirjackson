9cb6ea295dd0dac67c3a4e7bed1c324d5ecf33d8
Vincent Girard <vinccool96@gmail.com>
2024-06-08 15:36:34 -0400

    Implemented TSFBuilder

--- core/src/main/kotlin/org/cirjson/cirjackson/core/TSFBuilder.kt
+++ core/src/main/kotlin/org/cirjson/cirjackson/core/TSFBuilder.kt
@@ -70,4 +70,106 @@ abstract class TSFBuilder<F : TokenStreamFactory, B : TSFBuilder<F, B>> private 
     var formatWriteFeatures = formatWriteFeatures
         private set
 
+    fun enable(feature: TokenStreamFactory.Feature): B {
+        factoryFeatures = factoryFeatures or feature.mask
+        return getThis()
+    }
+
+    fun disable(feature: TokenStreamFactory.Feature): B {
+        factoryFeatures = factoryFeatures and feature.mask.inv()
+        return getThis()
+    }
+
+    fun configure(feature: TokenStreamFactory.Feature, isEnabled: Boolean): B {
+        return if (isEnabled) enable(feature) else disable(feature)
+    }
+
+    fun enable(feature: StreamReadFeature): B {
+        streamReadFeatures = streamReadFeatures or feature.mask
+        return getThis()
+    }
+
+    fun disable(feature: StreamReadFeature): B {
+        streamReadFeatures = streamReadFeatures and feature.mask.inv()
+        return getThis()
+    }
+
+    fun configure(feature: StreamReadFeature, isEnabled: Boolean): B {
+        return if (isEnabled) enable(feature) else disable(feature)
+    }
+
+    fun enable(feature: StreamWriteFeature): B {
+        streamWriteFeatures = streamWriteFeatures or feature.mask
+        return getThis()
+    }
+
+    fun disable(feature: StreamWriteFeature): B {
+        streamWriteFeatures = streamWriteFeatures and feature.mask.inv()
+        return getThis()
+    }
+
+    fun configure(feature: StreamWriteFeature, isEnabled: Boolean): B {
+        return if (isEnabled) enable(feature) else disable(feature)
+    }
+
+    /**
+     * Sets the constraints for streaming reads.
+     *
+     * @param streamReadConstraints constraints for streaming reads
+     *
+     * @return this builder
+     */
+    fun streamReadConstraints(streamReadConstraints: StreamReadConstraints): B {
+        this.streamReadConstraints = streamReadConstraints
+        return getThis()
+    }
+
+    /**
+     * Sets the constraints for streaming writes.
+     *
+     * @param streamWriteConstraints constraints for streaming writes
+     *
+     * @return this builder
+     */
+    fun streamWriteConstraints(streamWriteConstraints: StreamWriteConstraints): B {
+        this.streamWriteConstraints = streamWriteConstraints
+        return getThis()
+    }
+
+    /**
+     * Sets the configuration for error tokens.
+     *
+     * @param errorReportConfiguration configuration values used for handling errorneous token inputs.
+     *
+     * @return this builder
+     */
+    fun errorReportConfiguration(errorReportConfiguration: ErrorReportConfiguration): B {
+        this.errorReportConfiguration = errorReportConfiguration
+        return getThis()
+    }
+
+    /**
+     * Sets the recycler pool.
+     *
+     * @param recyclerPool RecyclerPool to use for buffer allocation
+     *
+     * @return this builder (for call chaining)
+     */
+    fun recyclerPool(recyclerPool: RecyclerPool<BufferRecycler>?): B {
+        this.recyclerPool = recyclerPool
+        return getThis()
+    }
+
+    /**
+     * Method for constructing actual [TokenStreamFactory] instance, given configuration.
+     *
+     * @return [TokenStreamFactory] build using builder configuration settings
+     */
+    abstract fun build(): F
+
+    @Suppress("UNCHECKED_CAST")
+    protected fun getThis(): B {
+        return this as B
+    }
+
 }
