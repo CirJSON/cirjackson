6c9a8c35cce4a195b7f8bae4231e020c85db3704
Vincent Girard <vinccool96@gmail.com>
2024-06-11 09:06:55 -0400

    Progress UTF8Writer

--- core/src/main/kotlin/org/cirjson/cirjackson/core/io/UTF8Writer.kt
+++ core/src/main/kotlin/org/cirjson/cirjackson/core/io/UTF8Writer.kt
@@ -12,36 +12,76 @@ class UTF8Writer(private val myContext: IOContext, private val myOutput: OutputS
 
     private var myOutputPointer = 0
 
+    private var mySurrogate = 0
+
+    @Throws(IOException::class)
     override fun close() {
         TODO("Not yet implemented")
     }
 
+    @Throws(IOException::class)
     override fun flush() {
         TODO("Not yet implemented")
     }
 
+    @Throws(IOException::class)
     override fun write(cbuf: CharArray, off: Int, len: Int) {
         TODO("Not yet implemented")
     }
 
+    @Throws(IOException::class)
     override fun write(c: Int) {
         TODO("Not yet implemented")
     }
 
+    @Throws(IOException::class)
     override fun write(cbuf: CharArray) {
         TODO("Not yet implemented")
     }
 
+    @Throws(IOException::class)
     override fun write(str: String) {
         TODO("Not yet implemented")
     }
 
+    @Throws(IOException::class)
     override fun write(str: String, off: Int, len: Int) {
         TODO("Not yet implemented")
     }
 
+    @Throws(IOException::class)
     override fun append(c: Char): Writer {
-        TODO("Not yet implemented")
+        write(c.code)
+        return this
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Internal methods
+     *******************************************************************************************************************
+     */
+
+    /**
+     * Method called to calculate Unicode code-point, from a surrogate pair.
+     *
+     * @param secondPart Second UTF-16 unit of surrogate (first part stored in `mySurrogate`)
+     *
+     * @return Decoded Unicode point
+     *
+     * @throws IOException If surrogate pair is invalid
+     */
+    @Throws(IOException::class)
+    private fun convertSurrogate(secondPart: Int): Int {
+        val firstPart = mySurrogate
+        mySurrogate = 0
+
+        if (secondPart !in SURR2_FIRST..SURR2_LAST) {
+            throw IOException("Broken surrogate pair: first char 0x${firstPart.toString(16)}), second 0x${
+                secondPart.toString(16)
+            }); illegal combination")
+        }
+
+        return (firstPart shl 10) + secondPart + SURROGATE_BASE
     }
 
     companion object {
@@ -62,7 +102,17 @@ class UTF8Writer(private val myContext: IOContext, private val myOutput: OutputS
         }
 
         internal fun illegalSurrogateDesc(code: Int): String {
-            TODO("Not yet implemented")
+            return if (code > 0x10FFFF) {
+                "Illegal character point (0x${code.toString(16)}) to output; max is 0x10FFFF as per CirJSON standards"
+            } else if (code >= SURR1_FIRST) {
+                if (code <= SURR1_LAST) {
+                    "Unmatched first part of surrogate pair (0x${code.toString(16)})"
+                } else {
+                    "Unmatched second part of surrogate pair (0x${code.toString(16)})"
+                }
+            } else {
+                "Illegal character point (0x${code.toString(16)}) to output"
+            }
         }
 
     }
