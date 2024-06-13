7b6f8988a5464d8bc83222d40561044945637847
Vincent Girard <vinccool96@gmail.com>
2024-06-13 10:32:11 -0400

    Implemented CirJsonStringEncoder

--- core/src/main/kotlin/org/cirjson/cirjackson/core/io/CirJsonStringEncoder.kt
+++ core/src/main/kotlin/org/cirjson/cirjackson/core/io/CirJsonStringEncoder.kt
@@ -1,6 +1,7 @@
 package org.cirjson.cirjackson.core.io
 
 import org.cirjson.cirjackson.core.util.ByteArrayBuilder
+import org.cirjson.cirjackson.core.util.TextBuffer
 import kotlin.math.max
 import kotlin.math.min
 
@@ -37,7 +38,63 @@ object CirJsonStringEncoder {
      * @return CirJSON-escaped String matching `input`
      */
     fun quoteAsCharArray(input: CharSequence): CharArray {
-        TODO()
+        val inputLength = input.length
+        var outputBuffer = CharArray(initialCharBufferSize(inputLength))
+        val escapeCodes = CharTypes.sevenBitOutputEscapes
+        val escapeCodeCount = escapeCodes.size
+        var inputPointer = 0
+        val textBufferDelegate = lazy { TextBuffer.fromInitial(outputBuffer) }
+        val textBuffer by textBufferDelegate
+        var outputPointer = 0
+        val qBuffer by lazy { qBuffer() }
+
+        outer@ while (inputPointer < inputLength) {
+            while (true) {
+                val c = input[inputPointer]
+
+                if (c.code < escapeCodeCount && escapeCodes[c.code] != 0) {
+                    break
+                }
+
+                if (outputPointer >= outputBuffer.size) {
+                    outputBuffer = textBuffer.finishCurrentSegment()
+                    outputPointer = 0
+                }
+
+                outputBuffer[outputPointer++] = c
+
+                if (++inputPointer >= inputLength) {
+                    break@outer
+                }
+            }
+
+            val d = input[inputPointer++]
+            val escapeCode = escapeCodes[d.code]
+            val length = if (escapeCode < 0) appendNumeric(d.code, qBuffer) else appendNamed(escapeCode, qBuffer)
+
+            if (outputPointer + length > outputBuffer.size) {
+                val first = outputBuffer.size - outputPointer
+
+                if (first > 0) {
+                    qBuffer.copyInto(outputBuffer, outputPointer, 0, first)
+                }
+
+                outputBuffer = textBuffer.finishCurrentSegment()
+                val second = length - first
+                qBuffer.copyInto(outputBuffer, 0, first, second + first)
+                outputPointer = second
+            } else {
+                qBuffer.copyInto(outputBuffer, outputPointer, 0, length)
+                outputPointer += length
+            }
+        }
+
+        if (!textBufferDelegate.isInitialized()) {
+            return outputBuffer.copyOfRange(0, outputPointer)
+        }
+
+        textBuffer.currentSegmentSize = outputPointer
+        return textBuffer.finishCurrentSegment()
     }
 
     /**
@@ -49,7 +106,32 @@ object CirJsonStringEncoder {
      * @param output [StringBuilder] to append escaped contents to
      */
     fun quoteAsString(input: CharSequence, output: StringBuilder) {
-        TODO()
+        val escapeCodes = CharTypes.sevenBitOutputEscapes
+        val escapeCodeCount = escapeCodes.size
+        var inputPointer = 0
+        val inputLength = input.length
+        val qBuffer by lazy { qBuffer() }
+
+        outer@ while (inputPointer < inputLength) {
+            while (true) {
+                val c = input[inputPointer]
+
+                if (c.code < escapeCodeCount && escapeCodes[c.code] != 0) {
+                    break
+                }
+
+                output.append(c)
+
+                if (++inputPointer >= inputLength) {
+                    break@outer
+                }
+            }
+
+            val d = input[inputPointer++]
+            val escapeCode = escapeCodes[d.code]
+            val length = if (escapeCode < 0) appendNumeric(d.code, qBuffer) else appendNamed(escapeCode, qBuffer)
+            output.appendRange(qBuffer, 0, length)
+        }
     }
 
     /**
@@ -61,7 +143,102 @@ object CirJsonStringEncoder {
      * @return UTF-8 encoded bytes of CirJSON-escaped `text`
      */
     fun quoteAsUTF8(text: CharSequence): ByteArray {
-        TODO()
+        var inputPointer = 0
+        val inputEnd = text.length
+        var outputPointer = 0
+        var outputBuffer = ByteArray(initialByteBufferSize(inputEnd))
+        val byteBuilderDelegate = lazy { ByteArrayBuilder.fromInitial(outputBuffer, outputPointer) }
+        val byteBuilder by byteBuilderDelegate
+
+        main@ while (inputPointer < inputEnd) {
+            val escapeCodes = CharTypes.sevenBitOutputEscapes
+
+            while (true) {
+                val ch = text[inputPointer]
+
+                if (ch.code > 0x7F || escapeCodes[ch.code] != 0) {
+                    break
+                }
+
+                if (outputPointer >= outputBuffer.size) {
+                    outputBuffer = byteBuilder.finishCurrentSegment()
+                    outputPointer = 0
+                }
+
+                outputBuffer[outputPointer++] = ch.code.toByte()
+
+                if (++inputPointer >= inputEnd) {
+                    break@main
+                }
+            }
+
+            if (outputPointer >= outputBuffer.size) {
+                outputBuffer = byteBuilder.finishCurrentSegment()
+                outputPointer = 0
+            }
+
+            var ch = text[inputPointer++].code
+
+            if (ch <= 0x7F) {
+                val escapeCode = escapeCodes[ch]
+                outputPointer = appendByte(ch, escapeCode, byteBuilder, outputPointer)
+                outputBuffer = byteBuilder.currentSegment!!
+                continue
+            }
+
+            if (ch <= 0x7FF) {
+                outputBuffer[outputPointer++] = (0xC0 or (ch shr 6)).toByte()
+            } else {
+                if (ch !in UTF8Writer.SURR1_FIRST..UTF8Writer.SURR2_FIRST) {
+                    outputBuffer[outputPointer++] = (0xE0 or (ch shr 12)).toByte()
+                } else {
+                    if (ch > UTF8Writer.SURR1_LAST) {
+                        illegal(ch)
+                    }
+
+                    if (inputPointer >= inputEnd) {
+                        illegal(ch)
+                    }
+
+                    ch = convert(ch, text[inputPointer++].code)
+
+                    if (ch > 0x10FFFF) {
+                        illegal(ch)
+                    }
+
+                    outputBuffer[outputPointer++] = (0xF0 or (ch shr 18)).toByte()
+
+                    if (outputPointer >= outputBuffer.size) {
+                        outputBuffer = byteBuilder.finishCurrentSegment()
+                        outputPointer = 0
+                    }
+
+                    outputBuffer[outputPointer++] = (0x80 or ((ch shr 12) and 0x3F)).toByte()
+                }
+
+                if (outputPointer >= outputBuffer.size) {
+                    outputBuffer = byteBuilder.finishCurrentSegment()
+                    outputPointer = 0
+                }
+
+                outputBuffer[outputPointer++] = (0x80 or ((ch shr 6) and 0x3F)).toByte()
+            }
+
+            ch = 0x80 or (ch and 0x3F)
+
+            if (outputPointer >= outputBuffer.size) {
+                outputBuffer = byteBuilder.finishCurrentSegment()
+                outputPointer = 0
+            }
+
+            outputBuffer[outputPointer++] = ch.toByte()
+        }
+
+        return if (!byteBuilderDelegate.isInitialized()) {
+            outputBuffer.copyOfRange(0, outputPointer)
+        } else {
+            byteBuilder.completeAndCoalesce(outputPointer)
+        }
     }
 
     /**
@@ -72,7 +249,93 @@ object CirJsonStringEncoder {
      * @return UTF-8 encoded bytes of `text` (without any escaping)
      */
     fun encodeAsUTF8(text: CharSequence): ByteArray {
-        TODO()
+        var inputPointer = 0
+        val inputEnd = text.length
+        var outputPointer = 0
+        var outputBuffer = ByteArray(initialByteBufferSize(inputEnd))
+        var outputEnd = outputBuffer.size
+        val byteBuilderDelegate = lazy { ByteArrayBuilder.fromInitial(outputBuffer, outputPointer) }
+        val byteBuilder by byteBuilderDelegate
+
+        main@ while (inputPointer < inputEnd) {
+            var c = text[inputPointer++].code
+
+            while (c <= 0x7F) {
+                if (outputPointer >= outputEnd) {
+                    outputBuffer = byteBuilder.finishCurrentSegment()
+                    outputEnd = outputBuffer.size
+                    outputPointer = 0
+                }
+
+                outputBuffer[outputPointer++] = c.toByte()
+
+                if (inputPointer >= inputEnd) {
+                    break@main
+                }
+
+                c = text[inputPointer++].code
+            }
+
+            if (outputPointer >= outputEnd) {
+                outputBuffer = byteBuilder.finishCurrentSegment()
+                outputEnd = outputBuffer.size
+                outputPointer = 0
+            }
+
+            if (c < 0x800) {
+                outputBuffer[outputPointer++] = (0xC0 or (c shr 6)).toByte()
+            } else {
+                if (c !in UTF8Writer.SURR1_FIRST..UTF8Writer.SURR2_FIRST) {
+                    outputBuffer[outputPointer++] = (0xE0 or (c shr 12)).toByte()
+                } else {
+                    if (c > UTF8Writer.SURR1_LAST) {
+                        illegal(c)
+                    }
+
+                    if (inputPointer >= inputEnd) {
+                        illegal(c)
+                    }
+
+                    c = convert(c, text[inputPointer++].code)
+
+                    if (c > 0x10FFFF) {
+                        illegal(c)
+                    }
+
+                    outputBuffer[outputPointer++] = (0xF0 or (c shr 18)).toByte()
+
+                    if (outputPointer >= outputBuffer.size) {
+                        outputBuffer = byteBuilder.finishCurrentSegment()
+                        outputEnd = outputBuffer.size
+                        outputPointer = 0
+                    }
+
+                    outputBuffer[outputPointer++] = (0x80 or ((c shr 12) and 0x3F)).toByte()
+                }
+
+                if (outputPointer >= outputBuffer.size) {
+                    outputBuffer = byteBuilder.finishCurrentSegment()
+                    outputEnd = outputBuffer.size
+                    outputPointer = 0
+                }
+
+                outputBuffer[outputPointer++] = (0x80 or ((c shr 6) and 0x3F)).toByte()
+            }
+
+            if (outputPointer >= outputBuffer.size) {
+                outputBuffer = byteBuilder.finishCurrentSegment()
+                outputEnd = outputBuffer.size
+                outputPointer = 0
+            }
+
+            outputBuffer[outputPointer++] = (0x80 or (c and 0x3F)).toByte()
+        }
+
+        return if (!byteBuilderDelegate.isInitialized()) {
+            outputBuffer.copyOfRange(0, outputPointer)
+        } else {
+            byteBuilder.completeAndCoalesce(outputPointer)
+        }
     }
 
     /*
