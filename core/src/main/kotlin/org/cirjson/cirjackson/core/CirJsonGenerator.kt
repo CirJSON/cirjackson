b213d22f9dd22b5fdda972e83271b4fd6c1b34ce
Vincent Girard <vinccool96@gmail.com>
2024-06-17 10:10:44 -0400

    Fixed doc

--- core/src/main/kotlin/org/cirjson/cirjackson/core/CirJsonGenerator.kt
+++ core/src/main/kotlin/org/cirjson/cirjackson/core/CirJsonGenerator.kt
@@ -4,7 +4,8 @@ import java.io.Closeable
 import java.io.Flushable
 
 /**
- * Base class that defines public API for writing CirJSON content. Instances are created using factory methods of a {@link TokenStreamFactory} instance.
+ * Base class that defines public API for writing CirJSON content. Instances are created using factory methods of a
+ * [TokenStreamFactory] instance.
  */
 abstract class CirJsonGenerator protected constructor() : Closeable, Flushable, Versioned {
 
@@ -15,23 +16,18 @@ abstract class CirJsonGenerator protected constructor() : Closeable, Flushable, 
      */
 
     /**
-     * Method called to close this generator, so that no more content
-     * can be written.
+     * Method called to close this generator, so that no more content can be written.
      *
-     * Whether the underlying target (stream, writer) gets closed depends
-     * on whether this generator either manages the target (i.e. is the
-     * only one with access to the target -- case if caller passes a
-     * reference to the resource such as File, but not stream); or
-     * has feature {@link StreamWriteFeature.AUTO_CLOSE_TARGET} enabled.
-     * If either of above is true, the target is also closed. Otherwise,
-     * (not managing, feature not enabled), target is not closed.
+     * Whether the underlying target (stream, writer) gets closed depends on whether this generator either manages the
+     * target (i.e. is the only one with access to the target -- case if caller passes a reference to the resource such
+     * as File, but not stream); or has feature [StreamWriteFeature.AUTO_CLOSE_TARGET] enabled. If either of above is
+     * true, the target is also closed. Otherwise, (not managing, feature not enabled), target is not closed.
      */
     abstract override fun close()
 
     /**
-     * Method called to flush any buffered content to the underlying
-     * target (output stream, writer), and to flush the target itself
-     * as well.
+     * Method called to flush any buffered content to the underlying target (output stream, writer), and to flush the
+     * target itself as well.
      */
     abstract override fun flush()
 
