ae80facbca3694053629f8730f4ffc6fe4af6a1c
Vincent Girard <vinccool96@gmail.com>
2024-07-09 09:41:39 -0400

    Implemented CirJsonParserBase

--- core/src/main/kotlin/org/cirjson/cirjackson/core/cirjson/CirJsonParserBase.kt
+++ core/src/main/kotlin/org/cirjson/cirjackson/core/cirjson/CirJsonParserBase.kt
@@ -2,8 +2,12 @@ package org.cirjson.cirjackson.core.cirjson
 
 import org.cirjson.cirjackson.core.*
 import org.cirjson.cirjackson.core.base.ParserBase
+import org.cirjson.cirjackson.core.exception.InputCoercionException
+import org.cirjson.cirjackson.core.exception.StreamReadException
 import org.cirjson.cirjackson.core.io.IOContext
+import org.cirjson.cirjackson.core.io.NumberInput
 import org.cirjson.cirjackson.core.util.CirJacksonFeatureSet
+import kotlin.math.max
 
 /**
  * Another intermediate base class, only used by actual CirJSON-backed parser implementations.
@@ -82,6 +86,245 @@ abstract class CirJsonParserBase(objectReadContext: ObjectReadContext, ioContext
             CirJsonToken.PROPERTY_NAME -> myIsNameCopied
             else -> false
         }
+
+    /*
+     *******************************************************************************************************************
+     * Numeric parsing method implementations
+     *******************************************************************************************************************
+     */
+
+    @Throws(CirJacksonException::class, InputCoercionException::class)
+    override fun parseNumericValue(numericType: Int) {
+        if (myCurrentToken == CirJsonToken.VALUE_NUMBER_FLOAT) {
+            parseSlowFloat(numericType)
+            return
+        }
+
+        if (myCurrentToken != CirJsonToken.VALUE_NUMBER_INT) {
+            throw constructNotNumericType(myCurrentToken!!, numericType)
+        }
+
+        val length = myIntLength
+
+        if (length <= 9) {
+            myNumberInt = myTextBuffer.contentAsInt(myNumberNegative)
+            myNumberTypesValid = NUMBER_INT
+            return
+        }
+
+        if (length <= 18) {
+            val long = myTextBuffer.contentAsLong(myNumberNegative)
+
+            if (length == 10) {
+                if (myNumberNegative) {
+                    if (long > LONG_MIN_INT) {
+                        myNumberInt = long.toInt()
+                        myNumberTypesValid = NUMBER_INT
+                        return
+                    }
+                } else if (long < LONG_MAX_INT) {
+                    myNumberInt = long.toInt()
+                    myNumberTypesValid = NUMBER_INT
+                    return
+                }
+            }
+
+            myNumberLong = long
+            myNumberTypesValid = NUMBER_LONG
+            return
+        }
+
+        if (length == 19) {
+            val buffer = myTextBuffer.textBuffer
+            var offset = myTextBuffer.textOffset
+
+            if (myNumberNegative) {
+                offset++
+            }
+
+            if (NumberInput.inLongRange(buffer, offset, length, myNumberNegative)) {
+                myNumberLong = NumberInput.parseLong19(buffer, offset, myNumberNegative)
+                myNumberTypesValid = NUMBER_LONG
+                return
+            }
+        }
+
+        parseSlowInt(numericType)
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun parseSlowFloat(numericType: Int) {
+        when (numericType) {
+            NUMBER_BIG_DECIMAL -> {
+                myNumberBigDecimal = null
+                myNumberString = myTextBuffer.contentsAsString()
+                myNumberTypesValid = NUMBER_BIG_DECIMAL
+            }
+
+            NUMBER_FLOAT -> {
+                myNumberFloat = 0.0f
+                myNumberString = myTextBuffer.contentsAsString()
+                myNumberTypesValid = NUMBER_FLOAT
+            }
+
+            else -> {
+                myNumberDouble = 0.0
+                myNumberString = myTextBuffer.contentsAsString()
+                myNumberTypesValid = NUMBER_DOUBLE
+            }
+        }
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun parseSlowInt(numericType: Int) {
+        val rawNumber = myTextBuffer.contentsAsString()
+
+        when (numericType) {
+            NUMBER_INT, NUMBER_LONG -> {
+                reportTooLongIntegral(numericType, rawNumber)
+            }
+
+            NUMBER_DOUBLE, NUMBER_FLOAT -> {
+                myNumberString = rawNumber
+                myNumberTypesValid = NUMBER_DOUBLE
+            }
+
+            else -> {
+                myNumberBigInteger = null
+                myNumberString = rawNumber
+                myNumberTypesValid = NUMBER_BIG_INTEGER
+            }
+        }
+    }
+
+    @Throws(CirJacksonException::class)
+    override fun parseIntValue(): Int {
+        if (myCurrentToken == CirJsonToken.VALUE_NUMBER_INT) {
+            if (myIntLength <= 9) {
+                val i = myTextBuffer.contentAsInt(myNumberNegative)
+                myNumberInt = i
+                myNumberTypesValid = NUMBER_INT
+                return i
+            }
+        }
+
+        parseNumericValue(NUMBER_INT)
+
+        if (myNumberTypesValid and NUMBER_INT == 0) {
+            convertNumberToInt()
+        }
+
+        return myNumberInt
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Internal/package methods: Configuration access
+     *******************************************************************************************************************
+     */
+
+    fun isEnabled(feature: CirJsonReadFeature): Boolean {
+        return feature.isEnabledIn(formatReadFeatures)
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Internal/package methods: Buffer handling
+     *******************************************************************************************************************
+     */
+
+    protected fun currentNameInBuffer(): CharArray {
+        if (myIsNameCopied) {
+            return myNameCopyBuffer
+        }
+
+        val name = streamReadContext!!.currentName!!
+        val nameLength = name.length
+
+        if (myNameCopyBuffer.size < nameLength) {
+            myNameCopyBuffer = CharArray(max(nameLength, 32))
+        }
+
+        name.toCharArray(myNameCopyBuffer, 0, 0, nameLength)
+        myIsNameCopied = true
+        return myNameCopyBuffer
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Internal/package methods: Error reporting
+     *******************************************************************************************************************
+     */
+
+    @Throws(CirJacksonException::class)
+    protected fun reportTooLongIntegral(numericType: Int, rawNumber: String) {
+        if (numericType == NUMBER_INT) {
+            reportOverflowInt(rawNumber)
+        } else {
+            reportOverflowLong(rawNumber)
+        }
+    }
+
+    @Throws(StreamReadException::class)
+    protected fun handleUnrecognizedCharacterEscape(char: Char): Char {
+        if (isEnabled(CirJsonReadFeature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER)) {
+            return char
+        }
+
+        if (char == '\'' && isEnabled(CirJsonReadFeature.ALLOW_SINGLE_QUOTES)) {
+            return char
+        }
+
+        throw constructReadException("Unrecognized character escape ${getCharDesc(char)}", currentLocationMinusOne())
+    }
+
+    @Throws(StreamReadException::class)
+    protected fun reportMismatchedEndMarker(actualChar: Char, expectedChar: Char) {
+        val context = streamReadContext
+        val message = "Unexpected close marker '$actualChar': expected '$expectedChar' (for ${
+            context!!.typeDescription
+        } starting at ${context.startLocation(contentReference())})"
+        throw constructReadException(message, currentLocationMinusOne())
+    }
+
+    /**
+     * Method called to report a problem with unquoted control character. Note: it is possible to suppress some
+     * instances of exception by enabling [CirJsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS].
+     */
+    @Throws(StreamReadException::class)
+    protected fun throwUnquotedSpace(code: Int, contextDescription: String) {
+        if (isEnabled(CirJsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS) && code <= CODE_SPACE) {
+            return
+        }
+
+        val char = code.toChar()
+        val message = "Illegal unquoted character (${
+            getCharDesc(char)
+        }): has to be escaped using backslash to be included in $contextDescription"
+        throw constructReadException(message, currentLocationMinusOne())
+    }
+
+    /**
+     * Description to use as "valid tokens" in an exception message about invalid (unrecognized) CirJSON token: called
+     * when parser finds something that looks like unquoted textual token
+     */
+    protected fun validCirJsonTokenList(): String {
+        return validCirJsonValueList()
+    }
+
+    /**
+     * Description to use as "valid CirJSON values" in an exception message about invalid (unrecognized) CirJSON value:
+     * called when parser finds something that does not look like a value or separator.
+     *
+     * @return The description to use
+     */
+    protected fun validCirJsonValueList(): String {
+        return if (isEnabled(CirJsonReadFeature.ALLOW_NON_NUMERIC_NUMBERS)) {
+            "(CirJSON String, Number (or 'NaN'/'+INF'/'-INF'), Array, Object or token 'null', 'true' or 'false')"
+        } else {
+            "(CirJSON String, Number, Array, Object or token 'null', 'true' or 'false')"
+        }
+    }
 
     companion object {
 
