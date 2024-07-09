bfd25cff5477db034465fc72aac4eae6f2696541
Vincent Girard <vinccool96@gmail.com>
2024-07-09 11:02:56 -0400

    Start classes

---
+++ core/src/main/kotlin/org/cirjson/cirjackson/core/cirjson/async/NonBlockingCirJsonParserBase.kt
@@ -0,0 +1,7 @@
+package org.cirjson.cirjackson.core.cirjson.async
+
+/**
+ * Intermediate base class for non-blocking CirJSON parsers.
+ */
+abstract class NonBlockingCirJsonParserBase {
+}
---
+++ core/src/main/kotlin/org/cirjson/cirjackson/core/filter/TokenFilter.kt
@@ -0,0 +1,8 @@
+package org.cirjson.cirjackson.core.filter
+
+/**
+ * Strategy class that can be implemented to specify actual inclusion/exclusion criteria for filtering, used by
+ * [FilteringGeneratorDelegate].
+ */
+open class TokenFilter {
+}
