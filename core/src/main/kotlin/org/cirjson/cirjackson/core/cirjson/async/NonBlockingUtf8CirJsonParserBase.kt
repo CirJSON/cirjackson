f1ad3cf77d4df41261bbec683829ce5d71d0ec56
Vincent Girard <vinccool96@gmail.com>
2024-07-12 13:10:52 -0400

    Progress NonBlockingUtf8CirJsonParserBase

--- core/src/main/kotlin/org/cirjson/cirjackson/core/cirjson/async/NonBlockingUtf8CirJsonParserBase.kt
+++ core/src/main/kotlin/org/cirjson/cirjackson/core/cirjson/async/NonBlockingUtf8CirJsonParserBase.kt
@@ -128,7 +128,7 @@ abstract class NonBlockingUtf8CirJsonParserBase(objectReadContext: ObjectReadCon
 
     /*
      *******************************************************************************************************************
-     * Second level decoding, primary field name decoding
+     * Second level decoding, value decoding
      *******************************************************************************************************************
      */
 
@@ -185,6 +185,286 @@ abstract class NonBlockingUtf8CirJsonParserBase(objectReadContext: ObjectReadCon
 
     @Throws(CirJacksonException::class)
     private fun finishCComment(fromMinorState: Int): CirJsonToken? {
+        TODO()
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Tertiary level decoding, simple tokens decoding
+     *******************************************************************************************************************
+     */
+
+    @Throws(CirJacksonException::class)
+    protected open fun startFalseToken(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun startTrueToken(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun startNullToken(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishKeywordToken(expectedToken: String, matched: Int, result: CirJsonToken): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishKeywordTokenWithEOF(expectedToken: String, matched: Int,
+            result: CirJsonToken): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishNonStdToken(expectedToken: String, matched: Int, result: CirJsonToken): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishNonStdTokenWithEOF(expectedToken: String, matched: Int,
+            result: CirJsonToken): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishErrorToken(): CirJsonToken {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishErrorTokenWithEOF(): CirJsonToken {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun reportErrorToken(actualToken: String): CirJsonToken {
+        return reportError("Unrecognized token '$actualToken': was expecting ${validCirJsonTokenList()}")
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Second level decoding, numbers decoding
+     *******************************************************************************************************************
+     */
+
+    @Throws(CirJacksonException::class)
+    protected open fun startFloatThatStartsWithPeriod(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun startPositiveNumber(code: Int): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun startNegativeNumber(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun startPositiveNumber(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun startNumberLeadingZero(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishNumberPlus(code: Int): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishNumberMinus(code: Int): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishNumberPlusMinus(code: Int, negative: Boolean): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishNumberLeadingZero(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishNumberPosLeadingZero(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishNumberNegLeadingZero(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishNumberPosNegLeadingZero(negative: Boolean): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishNumberIntegralPart(outputBuffer: CharArray, outputPointer: Int): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun startFloat(outputBuffer: CharArray, outputPointer: Int, code: Int): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishFloatFraction(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun finishFloatExponent(code: Int, checkSign: Boolean): CirJsonToken? {
+        TODO()
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Tertiary level decoding, name decoding
+     *******************************************************************************************************************
+     */
+
+    @Throws(CirJacksonException::class)
+    private fun fastParseName(): String? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun parseMediumName(pointer: Int, quad2: Int): String? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun parseMediumName(pointer: Int, quad3: Int, q2: Int): String? {
+        TODO()
+    }
+
+    /**
+     * Slower parsing method which is generally branched to when an escape sequence is detected (or alternatively for
+     * long names, one crossing input buffer boundary). Needs to be able to handle more exceptional cases, gets slower,
+     * and hence is offlined to a separate method.
+     */
+    @Throws(CirJacksonException::class)
+    private fun parseEscapedName(quadLength: Int, currentQuad: Int, currentQuadBytes: Int): String? {
+        TODO()
+    }
+
+    /**
+     * Method called when we see non-white space character other than double quote, when expecting a field name. In
+     * standard mode will just throw an exception; but in non-standard modes may be able to parse name.
+     */
+    @Throws(CirJacksonException::class)
+    private fun handleOddName(code: Int): CirJsonToken? {
+        TODO()
+    }
+
+    /**
+     * Parsing of optionally supported non-standard "unquoted" names: names without either double-quotes or apostrophes
+     * surrounding them. Unlike other
+     */
+    @Throws(CirJacksonException::class)
+    private fun finishUnquotedName(quadLength: Int, currentQuad: Int, currentQuadBytes: Int): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun finishApostropheName(quadLength: Int, currentQuad: Int, currentQuadBytes: Int): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun finishPropertyWithEscape(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun decodeSplitEscaped(value: Int, bytesRead: Int): Int {
+        TODO()
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Second level decoding, String decoding
+     *******************************************************************************************************************
+     */
+
+    @Throws(CirJacksonException::class)
+    protected open fun startString(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun finishRegularString(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    protected open fun startApostropheString(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun finishApostropheString(): CirJsonToken? {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun decodeSplitMultiByte(code: Int, type: Int, gotNext: Boolean): Boolean {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun decodeSplitUTF8V3(previous: Int, previousCount: Int, next: Int): Boolean {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun decodeSplitUTF8V4(previous: Int, previousCount: Int, next: Int): Boolean {
+        TODO()
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Internal methods, UTF8 decoding
+     *******************************************************************************************************************
+     */
+
+    @Throws(CirJacksonException::class)
+    private fun decodeCharEscape(): Int {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun decodeFastCharEscape(): Int {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun decodeUTF8V2(c: Int, d: Int): Int {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun decodeUTF8V3(c: Int, d: Int, e: Int): Int {
+        TODO()
+    }
+
+    @Throws(CirJacksonException::class)
+    private fun decodeUTF8V4(c: Int, d: Int, e: Int, f: Int): Int {
         TODO()
     }
 
