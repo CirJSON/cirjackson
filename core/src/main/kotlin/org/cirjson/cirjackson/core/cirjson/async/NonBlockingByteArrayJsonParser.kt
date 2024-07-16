8f90970e3ebebfe0608adf9263a9434ed1e2ceb0
Vincent Girard <vinccool96@gmail.com>
2024-07-16 12:34:13 -0400

    Completed NonBlockingByteArrayJsonParser and NonBlockingByteBufferJsonParser

---
+++ core/src/main/kotlin/org/cirjson/cirjackson/core/cirjson/async/NonBlockingByteArrayJsonParser.kt
@@ -0,0 +1,85 @@
+package org.cirjson.cirjackson.core.cirjson.async
+
+import org.cirjson.cirjackson.core.ObjectReadContext
+import org.cirjson.cirjackson.core.async.ByteArrayFeeder
+import org.cirjson.cirjackson.core.async.NonBlockingInputFeeder
+import org.cirjson.cirjackson.core.io.IOContext
+import org.cirjson.cirjackson.core.symbols.ByteQuadsCanonicalizer
+import java.io.IOException
+import java.io.OutputStream
+
+/**
+ * Non-blocking parser implementation for JSON content that takes its input via `ByteArray` passed.
+ *
+ * NOTE: only supports parsing of UTF-8 encoded content (and 7-bit US-ASCII since it is strict subset of UTF-8): other
+ * encodings are not supported.
+ */
+open class NonBlockingByteArrayJsonParser(objectReadContext: ObjectReadContext, ioContext: IOContext,
+        streamReadFeatures: Int, formatReadFeatures: Int, symbols: ByteQuadsCanonicalizer) :
+        NonBlockingUtf8CirJsonParserBase(objectReadContext, ioContext, streamReadFeatures, formatReadFeatures, symbols),
+        ByteArrayFeeder {
+
+    private var myInputBuffer = NO_BYTES
+
+    override fun nonBlockingInputFeeder(): NonBlockingInputFeeder {
+        return this
+    }
+
+    override val objectId: Any?
+        get() = null
+
+    override val nextSignedByteFromBuffer: Byte
+        get() = myInputBuffer[myInputPointer++]
+
+    override val nextUnsignedByteFromBuffer: Int
+        get() = myInputBuffer[myInputPointer++].toInt() and 0xFF
+
+
+    override fun getByteFromBuffer(pointer: Int): Byte {
+        return myInputBuffer[pointer]
+    }
+
+    override fun releaseBuffered(output: OutputStream): Int {
+        val avail = myInputEnd - myInputPointer
+
+        if (avail > 0) {
+            try {
+                output.write(myInputBuffer, myInputPointer, avail)
+            } catch (e: IOException) {
+                throw wrapIOFailure(e)
+            }
+        }
+
+        return avail
+    }
+
+    override val typeId: Any?
+        get() = null
+
+    override fun feedInput(data: ByteArray, offset: Int, end: Int) {
+        if (myInputPointer < myInputEnd) {
+            return reportError("Still have ${myInputEnd - myInputPointer} undecoded bytes, should not call 'feedInput'")
+        }
+
+        if (end < offset) {
+            return reportError("Input end ($end) may not be before start ($offset)")
+        }
+
+        if (myEndOfInput) {
+            return reportError("Already closed, can not feed more input")
+        }
+
+        myCurrentInputProcessed += myOriginalBufferLen
+
+        streamReadConstraints.validateDocumentLength(myCurrentInputProcessed)
+
+        myCurrentInputRowStart = offset - (myInputEnd - myCurrentInputRowStart)
+
+        myCurrentBufferStart = offset
+        myInputBuffer = data
+        myInputPointer = offset
+        myInputEnd = end
+        myOriginalBufferLen = end - offset
+    }
+
+}
---
+++ core/src/main/kotlin/org/cirjson/cirjackson/core/cirjson/async/NonBlockingByteBufferJsonParser.kt
@@ -0,0 +1,91 @@
+package org.cirjson.cirjackson.core.cirjson.async
+
+import org.cirjson.cirjackson.core.ObjectReadContext
+import org.cirjson.cirjackson.core.async.ByteBufferFeeder
+import org.cirjson.cirjackson.core.async.NonBlockingInputFeeder
+import org.cirjson.cirjackson.core.io.IOContext
+import org.cirjson.cirjackson.core.symbols.ByteQuadsCanonicalizer
+import java.io.IOException
+import java.io.OutputStream
+import java.nio.ByteBuffer
+import java.nio.channels.Channels
+
+/**
+ * Non-blocking parser implementation for JSON content that takes its input via [ByteBuffer] instance(s) passed.
+ *
+ * NOTE: only supports parsing of UTF-8 encoded content (and 7-bit US-ASCII since it is strict subset of UTF-8): other
+ * encodings are not supported.
+ */
+open class NonBlockingByteBufferJsonParser(objectReadContext: ObjectReadContext, ioContext: IOContext,
+        streamReadFeatures: Int, formatReadFeatures: Int, symbols: ByteQuadsCanonicalizer) :
+        NonBlockingUtf8CirJsonParserBase(objectReadContext, ioContext, streamReadFeatures, formatReadFeatures, symbols),
+        ByteBufferFeeder {
+
+    private var myInputBuffer = ByteBuffer.wrap(NO_BYTES)
+
+    override fun nonBlockingInputFeeder(): NonBlockingInputFeeder {
+        return this
+    }
+
+    override val objectId: Any?
+        get() = null
+
+    override val nextSignedByteFromBuffer: Byte
+        get() = myInputBuffer[myInputPointer++]
+
+    override val nextUnsignedByteFromBuffer: Int
+        get() = myInputBuffer[myInputPointer++].toInt() and 0xFF
+
+    override fun getByteFromBuffer(pointer: Int): Byte {
+        return myInputBuffer[pointer]
+    }
+
+    override fun releaseBuffered(output: OutputStream): Int {
+        val avail = myInputEnd - myInputPointer
+
+        if (avail > 0) {
+            val channel = Channels.newChannel(output)
+
+            try {
+                channel.write(myInputBuffer)
+            } catch (e: IOException) {
+                throw wrapIOFailure(e)
+            }
+        }
+
+        return avail
+    }
+
+    override val typeId: Any?
+        get() = null
+
+    override fun feedInput(buffer: ByteBuffer) {
+        if (myInputPointer < myInputEnd) {
+            return reportError("Still have ${myInputEnd - myInputPointer} undecoded bytes, should not call 'feedInput'")
+        }
+
+        val start = buffer.position()
+        val end = buffer.limit()
+
+        if (end < start) {
+            return reportError("Input end ($end) may not be before start ($start)")
+        }
+
+        if (myEndOfInput) {
+            return reportError("Already closed, can not feed more input")
+        }
+
+        myCurrentInputProcessed += myOriginalBufferLen
+
+        streamReadConstraints.validateDocumentLength(myCurrentInputProcessed)
+
+        myCurrentInputRowStart = start - (myInputEnd - myCurrentInputRowStart)
+
+        myCurrentBufferStart = start
+        myInputBuffer = buffer
+        myInputPointer = start
+        myInputEnd = end
+        myOriginalBufferLen = end - start
+    }
+
+}
