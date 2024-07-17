b4aa7e174ff180f0a865dc62f5e1e8bdd0d78572
Vincent Girard <vinccool96@gmail.com>
2024-07-17 10:46:39 -0400

    Progress ByteSourceCirJsonBootstrapper

--- core/src/main/kotlin/org/cirjson/cirjackson/core/CirJsonEncoding.kt
+++ core/src/main/kotlin/org/cirjson/cirjackson/core/CirJsonEncoding.kt
@@ -4,6 +4,12 @@ enum class CirJsonEncoding(val javaName: String) {
 
     UTF8("UTF-8"),
 
-    UTF16_BE("UTF-16BE");
+    UTF16_BE("UTF-16BE"),
+
+    UTF16_LE("UTF-16LE"),
+
+    UTF32_BE("UTF-32BE"),
+
+    UTF32_LE("UTF-32LE");
 
 }
--- core/src/main/kotlin/org/cirjson/cirjackson/core/cirjson/ByteSourceCirJsonBootstrapper.kt
+++ core/src/main/kotlin/org/cirjson/cirjackson/core/cirjson/ByteSourceCirJsonBootstrapper.kt
@@ -1,11 +1,14 @@
 package org.cirjson.cirjackson.core.cirjson
 
 import org.cirjson.cirjackson.core.CirJacksonException
+import org.cirjson.cirjackson.core.CirJsonEncoding
 import org.cirjson.cirjackson.core.exception.CirJacksonIOException
 import org.cirjson.cirjackson.core.io.IOContext
+import org.cirjson.cirjackson.core.util.Other
 import java.io.DataInput
 import java.io.IOException
 import java.io.InputStream
+import java.io.Reader
 
 /**
  * This class is used to determine the encoding of byte stream that is to contain CirJSON content. Rules are fairly
@@ -49,6 +52,76 @@ class ByteSourceCirJsonBootstrapper {
         myInputPointer = inputStart
         myInputEnd = inputLength + inputStart
         myIsBufferRecyclable = false
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Encoding detection during bootstrapping
+     *******************************************************************************************************************
+     */
+
+    /**
+     * Method that should be called after constructing an instance. It will figure out encoding that content uses, to
+     * allow for instantiating a proper scanner object.
+     *
+     * @return [CirJsonEncoding] detected, if any; [CirJsonEncoding.UTF8] otherwise
+     *
+     * @throws CirJacksonException If read from underlying input source fails
+     */
+    @Throws(CirJacksonException::class)
+    fun detectEncoding(): CirJsonEncoding {
+        var foundEncoding = false
+
+        if (ensureLoaded(4)) {
+            val quad = myInputBuffer[myInputPointer].toInt() shl 24 or
+                    (myInputBuffer[myInputPointer + 1].toInt() and 0xFF shl 16) or
+                    (myInputBuffer[myInputPointer + 2].toInt() and 0xFF shl 8) or
+                    (myInputBuffer[myInputPointer + 3].toInt() and 0xFF)
+
+            if (handleBOM(quad)) {
+                foundEncoding = true
+            } else {
+                if (checkUTF32(quad)) {
+                    foundEncoding = true
+                } else if (checkUTF16(quad ushr 16)) {
+                    foundEncoding = true
+                }
+            }
+        } else if (ensureLoaded(2)) {
+            val i16 = myInputBuffer[myInputPointer].toInt() and 0xFF shl 8 or
+                    (myInputBuffer[myInputPointer + 1].toInt() and 0xFF)
+
+            if (checkUTF16(i16)) {
+                foundEncoding = true
+            }
+        }
+
+        val encoding = if (foundEncoding) {
+            when (myBytesPerChar) {
+                1 -> CirJsonEncoding.UTF8
+                2 -> if (myIsBigEndian) CirJsonEncoding.UTF16_BE else CirJsonEncoding.UTF16_LE
+                4 -> if (myIsBigEndian) CirJsonEncoding.UTF32_BE else CirJsonEncoding.UTF32_LE
+                else -> Other.throwInternalReturnAny()
+            }
+        } else {
+            CirJsonEncoding.UTF8
+        }
+
+        myContext.encoding = encoding
+        return encoding
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Constructing a Reader
+     *******************************************************************************************************************
+     */
+
+    @Throws(CirJacksonException::class)
+    fun constructReader(): Reader {
+        val encoding = myContext.encoding!!
+
+        TODO()
     }
 
     /*
