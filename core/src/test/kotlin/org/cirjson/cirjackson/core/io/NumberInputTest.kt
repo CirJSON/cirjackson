b863d828cd13dd4fc780e443d952ce098ab84251
Vincent Girard <vinccool96@gmail.com>
2024-09-21 10:35:36 -0400

    NumberInput tests

--- core/src/test/kotlin/org/cirjson/cirjackson/core/io/NumberInputTest.kt
+++ core/src/test/kotlin/org/cirjson/cirjackson/core/io/NumberInputTest.kt
@@ -104,4 +104,137 @@ class NumberInputTest : TestBase() {
         assertFalse(NumberInput.looksLikeValidNumber("10_000"))
     }
 
+    @Test
+    fun testIntParsing() {
+        var testChars = "123456789".toCharArray()
+
+        assertEquals(3, NumberInput.parseInt(testChars, 2, 1))
+        assertEquals(123, NumberInput.parseInt(testChars, 0, 3))
+        assertEquals(2345, NumberInput.parseInt(testChars, 1, 4))
+        assertEquals(9, NumberInput.parseInt(testChars, 8, 1))
+        assertEquals(456789, NumberInput.parseInt(testChars, 3, 6))
+        assertEquals(23456, NumberInput.parseInt(testChars, 1, 5))
+        assertEquals(123456789, NumberInput.parseInt(testChars, 0, 9))
+
+        testChars = "32".toCharArray()
+        assertEquals(32, NumberInput.parseInt(testChars, 0, 2))
+        testChars = "189".toCharArray()
+        assertEquals(189, NumberInput.parseInt(testChars, 0, 3))
+
+        testChars = "10".toCharArray()
+        assertEquals(10, NumberInput.parseInt(testChars, 0, 2))
+        assertEquals(0, NumberInput.parseInt(testChars, 1, 1))
+    }
+
+    @Test
+    fun testIntParsingWithStrings() {
+        assertEquals(3, NumberInput.parseInt("3"))
+        assertEquals(3, NumberInput.parseInt("+3"))
+        assertEquals(0, NumberInput.parseInt("0"))
+        assertEquals(-3, NumberInput.parseInt("-3"))
+        assertEquals(27, NumberInput.parseInt("27"))
+        assertEquals(-31, NumberInput.parseInt("-31"))
+        assertEquals(271, NumberInput.parseInt("271"))
+        assertEquals(-131, NumberInput.parseInt("-131"))
+        assertEquals(2709, NumberInput.parseInt("2709"))
+        assertEquals(-9999, NumberInput.parseInt("-9999"))
+        assertEquals(Int.MIN_VALUE, NumberInput.parseInt("${Int.MIN_VALUE}"))
+        assertEquals(Int.MAX_VALUE, NumberInput.parseInt("${Int.MAX_VALUE}"))
+    }
+
+    @Test
+    fun testLongParsing() {
+        val testChars = "123456789012345678".toCharArray()
+
+        assertEquals(123456789012345678L, NumberInput.parseLong(testChars, 0, testChars.size))
+    }
+
+    @Test
+    fun testLongParsingWithStrings() {
+        assertEquals(3, NumberInput.parseLong("3"))
+        assertEquals(3, NumberInput.parseLong("+3"))
+        assertEquals(0, NumberInput.parseLong("0"))
+        assertEquals(-3, NumberInput.parseLong("-3"))
+        assertEquals(27, NumberInput.parseLong("27"))
+        assertEquals(-31, NumberInput.parseLong("-31"))
+        assertEquals(271, NumberInput.parseLong("271"))
+        assertEquals(-131, NumberInput.parseLong("-131"))
+        assertEquals(2709, NumberInput.parseLong("2709"))
+        assertEquals(-9999, NumberInput.parseLong("-9999"))
+        assertEquals(Long.MIN_VALUE, NumberInput.parseLong("${Long.MIN_VALUE}"))
+        assertEquals(Long.MAX_VALUE, NumberInput.parseLong("${Long.MAX_VALUE}"))
+        assertEquals(Int.MIN_VALUE - 1L, NumberInput.parseLong("${Int.MIN_VALUE - 1L}"))
+        assertEquals(Int.MAX_VALUE + 1L, NumberInput.parseLong("${Int.MAX_VALUE + 1L}"))
+    }
+
+    @Test
+    fun testLongBoundsChecks() {
+        val minLong = Long.MIN_VALUE.toString().substring(1)
+        val belowMinLong = BigInteger.valueOf(Long.MIN_VALUE).subtract(BigInteger.ONE).toString().substring(1)
+        val maxLong = Long.MAX_VALUE.toString()
+        val aboveMaxLong = BigInteger.valueOf(Long.MAX_VALUE).add(BigInteger.ONE).toString()
+        val otherValue = "1323372036854775807"
+        val overflow = "9999999999999999999"
+
+        assertTrue(NumberInput.inLongRange(minLong, true))
+        assertTrue(NumberInput.inLongRange(maxLong, false))
+        assertTrue(NumberInput.inLongRange(otherValue, true))
+        assertTrue(NumberInput.inLongRange(otherValue, false))
+
+        assertFalse(NumberInput.inLongRange(overflow, true))
+        assertFalse(NumberInput.inLongRange(overflow, false))
+        assertFalse(NumberInput.inLongRange(belowMinLong, true))
+        assertFalse(NumberInput.inLongRange(aboveMaxLong, false))
+
+        var chars = minLong.toCharArray()
+        assertTrue(NumberInput.inLongRange(chars, 0, chars.size, true))
+        chars = maxLong.toCharArray()
+        assertTrue(NumberInput.inLongRange(chars, 0, chars.size, false))
+        chars = otherValue.toCharArray()
+        assertTrue(NumberInput.inLongRange(chars, 0, chars.size, true))
+        assertTrue(NumberInput.inLongRange(chars, 0, chars.size, false))
+
+        chars = belowMinLong.toCharArray()
+        assertFalse(NumberInput.inLongRange(chars, 0, chars.size, true))
+        chars = aboveMaxLong.toCharArray()
+        assertFalse(NumberInput.inLongRange(chars, 0, chars.size, false))
+        chars = overflow.toCharArray()
+        assertFalse(NumberInput.inLongRange(chars, 0, chars.size, true))
+        assertFalse(NumberInput.inLongRange(chars, 0, chars.size, false))
+    }
+
+    @Test
+    fun testIntOverflow() {
+        try {
+            NumberInput.parseInt("2147483648")
+            fail("Should have thrown an exception")
+        } catch (e: NumberFormatException) {
+            verifyException(e, "For input string: \"2147483648\"")
+        }
+
+        try {
+            NumberInput.parseInt("-2147483649")
+            fail("Should have thrown an exception")
+        } catch (e: NumberFormatException) {
+            verifyException(e, "For input string: \"-2147483649\"")
+        }
+    }
+
+    @Test
+    fun testLongOverflow() {
+        try {
+            NumberInput.parseInt("9223372036854775808")
+            fail("Should have thrown an exception")
+        } catch (e: NumberFormatException) {
+            verifyException(e, "For input string: \"9223372036854775808\"")
+        }
+
+        try {
+            NumberInput.parseInt("-9223372036854775809")
+            fail("Should have thrown an exception")
+        } catch (e: NumberFormatException) {
+            verifyException(e, "For input string: \"-9223372036854775809\"")
+        }
+    }
+
 }
