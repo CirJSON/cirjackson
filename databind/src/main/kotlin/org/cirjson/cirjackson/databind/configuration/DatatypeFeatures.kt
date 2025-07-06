6639bcf257178b2b7a486474e1d076ba2694c680
Vincent Girard <vinccool96@gmail.com>
2025-07-06 11:12:33 -0400

    Forgotten constructor

--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/configuration/DatatypeFeatures.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/configuration/DatatypeFeatures.kt
@@ -1,6 +1,7 @@
 package org.cirjson.cirjackson.databind.configuration
 
-open class DatatypeFeatures {
+open class DatatypeFeatures protected constructor(private val myEnabledFor1: Int, private val myEnabledFor2: Int,
+        private val myExplicitFor1: Int, private val myExplicitFor2: Int) {
 
     companion object {
 
