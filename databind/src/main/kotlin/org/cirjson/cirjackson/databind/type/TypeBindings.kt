953f0343c6c33a8272449c3fe4525128302d54ef
Vincent Girard <vinccool96@gmail.com>
2025-08-10 12:44:00 -0400

    Made TypeBindings open

--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/type/TypeBindings.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/type/TypeBindings.kt
@@ -2,7 +2,7 @@ package org.cirjson.cirjackson.databind.type
 
 import org.cirjson.cirjackson.databind.KotlinType
 
-class TypeBindings private constructor(names: Array<String>?, types: Array<KotlinType>?,
+open class TypeBindings private constructor(names: Array<String>?, types: Array<KotlinType>?,
         private val myUnboundVariables: Array<String>?) {
 
     companion object {
