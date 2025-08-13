5f5b380802bcaa79ed9bc9aa6e9ed1edd688fb44
Vincent Girard <vinccool96@gmail.com>
2025-08-13 12:58:16 -0400

    Added IdentityEqualityType and SimpleType

--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/type/IdentityEqualityType.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/type/IdentityEqualityType.kt
@@ -3,17 +3,20 @@ package org.cirjson.cirjackson.databind.type
 import org.cirjson.cirjackson.databind.KotlinType
 import kotlin.reflect.KClass
 
+/**
+ * Internal abstract type representing [TypeBase] implementations which use reference equality.
+ */
 abstract class IdentityEqualityType protected constructor(raw: KClass<*>, bindings: TypeBindings?,
-        superClass: KotlinType?, interfaces: List<KotlinType>?, additionalHash: Int, valueHandler: Any?,
+        superClass: KotlinType?, interfaces: Array<KotlinType>?, additionalHash: Int, valueHandler: Any?,
         typeHandler: Any?, isUsedAsStaticType: Boolean) :
         TypeBase(raw, bindings, superClass, interfaces, additionalHash, valueHandler, typeHandler, isUsedAsStaticType) {
 
-    override fun equals(other: Any?): Boolean {
-        TODO("Not yet implemented")
+    final override fun equals(other: Any?): Boolean {
+        return other === this
     }
 
     override fun hashCode(): Int {
-        TODO("Not yet implemented")
+        return System.identityHashCode(this)
     }
 
 }
--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/type/SimpleType.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/type/SimpleType.kt
@@ -3,10 +3,13 @@ package org.cirjson.cirjackson.databind.type
 import org.cirjson.cirjackson.databind.KotlinType
 import kotlin.reflect.KClass
 
+/**
+ * Simple types are defined as anything other than one of recognized container types (arrays, Collections, Maps). For
+ * our needs we need not know anything further, since we have no way of dealing with generic types other than
+ * Collections and Maps.
+ */
 @Suppress("EqualsOrHashCode")
 open class SimpleType : TypeBase {
-
-    protected constructor(base: TypeBase) : super(base)
 
     /*
      *******************************************************************************************************************
@@ -14,33 +17,98 @@ open class SimpleType : TypeBase {
      *******************************************************************************************************************
      */
 
+    /**
+     * Constructor only used by core CirJackson databind functionality; should never be called by application code.
+     *
+     * As with other direct construction that by-passes [TypeFactory], no introspection occurs with respect to
+     * super-types; caller must be aware of consequences if using this method.
+     */
+    protected constructor(clazz: KClass<*>) : this(clazz, TypeBindings.EMPTY, null, null)
+
+    protected constructor(clazz: KClass<*>, bindings: TypeBindings?, superClass: KotlinType?,
+            superInterfaces: Array<KotlinType>?) : this(clazz, bindings, superClass, superInterfaces, null, null, false)
+
+    /**
+     * Simple copy-constructor, usually used when upgrading/refining a simple type into more specialized type.
+     */
+    protected constructor(base: TypeBase) : super(base)
+
+    protected constructor(clazz: KClass<*>, bindings: TypeBindings?, superClass: KotlinType?,
+            superInterfaces: Array<KotlinType>?, valueHandler: Any?, typeHandler: Any?,
+            isUsedAsStaticType: Boolean) : super(clazz, bindings, superClass, superInterfaces,
+            (bindings ?: TypeBindings.EMPTY).hashCode(), valueHandler, typeHandler, isUsedAsStaticType)
+
+    protected constructor(clazz: KClass<*>, bindings: TypeBindings?, superClass: KotlinType?,
+            superInterfaces: Array<KotlinType>?, extraHash: Int, valueHandler: Any?, typeHandler: Any?,
+            isUsedAsStaticType: Boolean) : super(clazz, bindings, superClass, superInterfaces, extraHash, valueHandler,
+            typeHandler, isUsedAsStaticType)
+
     override fun withContentType(contentType: KotlinType): KotlinType {
-        TODO("Not yet implemented")
+        throw IllegalArgumentException("Simple types have no content types; cannot call withContentType()")
     }
 
     override fun withTypeHandler(handler: Any?): SimpleType {
-        TODO("Not yet implemented")
+        if (myTypeHandler === handler) {
+            return this
+        }
+
+        return SimpleType(myClass, myBindings, mySuperClass, mySuperInterfaces, myValueHandler, handler,
+                isUsedAsStaticType)
     }
 
     override fun withContentTypeHandler(handler: Any?): KotlinType {
-        TODO("Not yet implemented")
+        throw IllegalArgumentException("Simple types have no content types; cannot call withContentTypeHandler()")
     }
 
     override fun withValueHandler(handler: Any?): SimpleType {
-        TODO("Not yet implemented")
+        if (myValueHandler === handler) {
+            return this
+        }
+
+        return SimpleType(myClass, myBindings, mySuperClass, mySuperInterfaces, handler, myTypeHandler,
+                isUsedAsStaticType)
     }
 
     override fun withContentValueHandler(handler: Any?): SimpleType {
-        TODO("Not yet implemented")
+        throw IllegalArgumentException("Simple types have no content types; cannot call withContentValueHandler()")
     }
 
     override fun withStaticTyping(): SimpleType {
-        TODO("Not yet implemented")
+        if (isUsedAsStaticType) {
+            return this
+        }
+
+        return SimpleType(myClass, myBindings, mySuperClass, mySuperInterfaces, myValueHandler, myTypeHandler, true)
     }
 
     override fun refine(raw: KClass<*>, bindings: TypeBindings, superClass: KotlinType,
             superInterfaces: Array<KotlinType>): KotlinType? {
-        TODO("Not yet implemented")
+        return null
+    }
+
+    override fun buildCanonicalName(): String {
+        val stringBuilder = StringBuilder()
+        stringBuilder.append(myClass.qualifiedName)
+
+        val count = myBindings.size
+
+        if (count > 0 && hasNTypeParameters(count)) {
+            stringBuilder.append('<')
+
+            for (i in 0..<count) {
+                val type = containedType(i)!!
+
+                if (i > 0) {
+                    stringBuilder.append(',')
+                }
+
+                stringBuilder.append(type.toCanonical())
+            }
+
+            stringBuilder.append('>')
+        }
+
+        return stringBuilder.toString()
     }
 
     /*
@@ -51,12 +119,31 @@ open class SimpleType : TypeBase {
 
     override val isContainerType = false
 
+    override fun hasContentType(): Boolean {
+        return false
+    }
+
     override fun getErasedSignature(stringBuilder: StringBuilder): StringBuilder {
-        TODO("Not yet implemented")
+        return classSignature(myClass, stringBuilder, true)
     }
 
     override fun getGenericSignature(stringBuilder: StringBuilder): StringBuilder {
-        TODO("Not yet implemented")
+        classSignature(myClass, stringBuilder, false)
+
+        val count = myBindings.size
+
+        if (count > 0) {
+            stringBuilder.append('<')
+
+            for (i in 0..<count) {
+                containedType(i)!!.getErasedSignature(stringBuilder)
+            }
+
+            stringBuilder.append('>')
+        }
+
+        stringBuilder.append(';')
+        return stringBuilder
     }
 
     /*
@@ -66,11 +153,36 @@ open class SimpleType : TypeBase {
      */
 
     override fun toString(): String {
-        TODO("Not yet implemented")
+        val stringBuilder = StringBuilder(40)
+        stringBuilder.append("[simple type, class ").append(buildCanonicalName()).append(']')
+        return stringBuilder.toString()
     }
 
     override fun equals(other: Any?): Boolean {
-        TODO("Not yet implemented")
+        if (this === other) {
+            return true
+        }
+
+        if (other !is SimpleType || this::class != other::class) {
+            return false
+        }
+
+        return myClass == other.myClass && myBindings == other.myBindings
+    }
+
+    companion object {
+
+        /**
+         * Method used by core Jackson classes: NOT to be used by application code: it does NOT properly handle
+         * inspection of super-types, so neither parent Classes nor implemented Interfaces are accessible with resulting
+         * type instance.
+         *
+         * NOTE: public only because it is called by `ObjectMapper` which is not in same package
+         */
+        fun constructUnsafe(raw: KClass<*>): SimpleType {
+            return SimpleType(raw, null, null, null, null, null, false)
+        }
+
     }
 
 }
