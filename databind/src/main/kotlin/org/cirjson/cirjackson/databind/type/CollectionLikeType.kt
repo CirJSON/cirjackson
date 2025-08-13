9738554434984c75a77d1c8eb1313d1afef42295
Vincent Girard <vinccool96@gmail.com>
2025-08-13 12:18:55 -0400

    Added CollectionLikeType and CollectionType

--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/type/CollectionLikeType.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/type/CollectionLikeType.kt
@@ -3,6 +3,11 @@ package org.cirjson.cirjackson.databind.type
 import org.cirjson.cirjackson.databind.KotlinType
 import kotlin.reflect.KClass
 
+/**
+ * Type that represents things that act similar to [Collection]; but may or may not be instances of that interface. This
+ * specifically allows framework to check for configuration and annotation settings used for Map types, and pass these
+ * to custom handlers that may be more familiar with actual type.
+ */
 @Suppress("EqualsOrHashCode")
 open class CollectionLikeType : TypeBase {
 
@@ -26,32 +31,55 @@ open class CollectionLikeType : TypeBase {
     }
 
     override fun withContentType(contentType: KotlinType): KotlinType {
-        TODO("Not yet implemented")
+        if (myElementType === contentType) {
+            return this
+        }
+
+        return CollectionLikeType(myClass, myBindings, mySuperClass, mySuperInterfaces, contentType, myValueHandler,
+                myTypeHandler, isUsedAsStaticType)
     }
 
     override fun withTypeHandler(handler: Any?): CollectionLikeType {
-        TODO("Not yet implemented")
+        return CollectionLikeType(myClass, myBindings, mySuperClass, mySuperInterfaces, myElementType, myValueHandler,
+                handler, isUsedAsStaticType)
     }
 
     override fun withContentTypeHandler(handler: Any?): CollectionLikeType {
-        TODO("Not yet implemented")
+        return CollectionLikeType(myClass, myBindings, mySuperClass, mySuperInterfaces,
+                myElementType.withTypeHandler(handler), myValueHandler, myTypeHandler, isUsedAsStaticType)
     }
 
     override fun withValueHandler(handler: Any?): CollectionLikeType {
-        TODO("Not yet implemented")
+        return CollectionLikeType(myClass, myBindings, mySuperClass, mySuperInterfaces, myElementType, handler,
+                myTypeHandler, isUsedAsStaticType)
     }
 
     override fun withContentValueHandler(handler: Any?): CollectionLikeType {
-        TODO("Not yet implemented")
+        return CollectionLikeType(myClass, myBindings, mySuperClass, mySuperInterfaces,
+                myElementType.withValueHandler(handler), myValueHandler, myTypeHandler, isUsedAsStaticType)
+    }
+
+    override fun withHandlersFrom(src: KotlinType): KotlinType {
+        val type = super.withHandlersFrom(src)
+        val sourceContentType = src.contentType ?: return type
+        val contentType = myElementType.withHandlersFrom(sourceContentType)
+
+        return type.takeUnless { contentType === myElementType } ?: type.withContentType(contentType)
     }
 
     override fun withStaticTyping(): CollectionLikeType {
-        TODO("Not yet implemented")
+        if (isUsedAsStaticType) {
+            return this
+        }
+
+        return CollectionLikeType(myClass, myBindings, mySuperClass, mySuperInterfaces,
+                myElementType.withStaticTyping(), myValueHandler, myTypeHandler, true)
     }
 
     override fun refine(raw: KClass<*>, bindings: TypeBindings, superClass: KotlinType,
-            superInterfaces: Array<KotlinType>): KotlinType? {
-        TODO("Not yet implemented")
+            superInterfaces: Array<KotlinType>): KotlinType {
+        return CollectionLikeType(raw, bindings, superClass, superInterfaces, myElementType, myValueHandler,
+                myTypeHandler, isUsedAsStaticType)
     }
 
     /*
@@ -60,14 +88,46 @@ open class CollectionLikeType : TypeBase {
      *******************************************************************************************************************
      */
 
-    override val isContainerType = false
+    override val isContainerType = true
+
+    override val isCollectionLikeType: Boolean = true
+
+    override val contentType: KotlinType
+        get() = myElementType
+
+    override val contentValueHandler: Any?
+        get() = myElementType.valueHandler
+
+    override val contentTypeHandler: Any?
+        get() = myElementType.typeHandler
+
+    override fun hasHandlers(): Boolean {
+        return super.hasHandlers() || myElementType.hasHandlers()
+    }
 
     override fun getErasedSignature(stringBuilder: StringBuilder): StringBuilder {
-        TODO("Not yet implemented")
+        return classSignature(myClass, stringBuilder, true)
     }
 
     override fun getGenericSignature(stringBuilder: StringBuilder): StringBuilder {
-        TODO("Not yet implemented")
+        classSignature(myClass, stringBuilder, false)
+        stringBuilder.append('<')
+        myElementType.getGenericSignature(stringBuilder)
+        stringBuilder.append(">;")
+        return stringBuilder
+    }
+
+    override fun buildCanonicalName(): String {
+        val stringBuilder = StringBuilder()
+        stringBuilder.append(myClass.qualifiedName)
+
+        if (hasNTypeParameters(1)) {
+            stringBuilder.append('<')
+            stringBuilder.append(myElementType.toCanonical())
+            stringBuilder.append('>')
+        }
+
+        return stringBuilder.toString()
     }
 
     /*
@@ -77,11 +137,40 @@ open class CollectionLikeType : TypeBase {
      */
 
     override fun toString(): String {
-        TODO("Not yet implemented")
+        return "[collection-like type; class ${myClass.qualifiedName}, contains $myElementType]"
     }
 
     override fun equals(other: Any?): Boolean {
-        TODO("Not yet implemented")
+        if (this === other) {
+            return true
+        }
+
+        if (other !is CollectionLikeType || other::class != this::class) {
+            return false
+        }
+
+        return myClass == other.myClass && myElementType == other.myElementType
+    }
+
+    companion object {
+
+        fun construct(collectionType: KClass<*>, bindings: TypeBindings?, superClass: KotlinType?,
+                interfaces: Array<KotlinType>?, elementType: KotlinType): CollectionLikeType {
+            return CollectionLikeType(collectionType, bindings, superClass, interfaces, elementType, null, null, false)
+        }
+
+        /**
+         * Factory method that can be used to "upgrade" a basic type into collection-like one; usually done via
+         * [TypeModifier].
+         */
+        fun upgradeFrom(baseType: KotlinType, elementType: KotlinType): CollectionLikeType {
+            if (baseType !is TypeBase) {
+                throw IllegalArgumentException("Cannot upgrade from an instance of ${baseType::class}")
+            }
+
+            return CollectionLikeType(baseType, elementType)
+        }
+
     }
 
 }
--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/type/CollectionType.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/type/CollectionType.kt
@@ -3,9 +3,82 @@ package org.cirjson.cirjackson.databind.type
 import org.cirjson.cirjackson.databind.KotlinType
 import kotlin.reflect.KClass
 
+/**
+ * Type that represents Kotlin Collection types (Lists, Sets).
+ */
 class CollectionType private constructor(collectionType: KClass<*>, bindings: TypeBindings?, superClass: KotlinType?,
         interfaces: Array<KotlinType>?, elementType: KotlinType, valueHandler: Any?, typeHandler: Any?,
         isUsedAsStaticType: Boolean) :
         CollectionLikeType(collectionType, bindings, superClass, interfaces, elementType, valueHandler, typeHandler,
                 isUsedAsStaticType) {
+
+    /*
+     *******************************************************************************************************************
+     * Lifecycle
+     *******************************************************************************************************************
+     */
+
+    override fun withContentType(contentType: KotlinType): KotlinType {
+        if (myElementType === contentType) {
+            return this
+        }
+
+        return CollectionType(myClass, myBindings, mySuperClass, mySuperInterfaces, contentType, myValueHandler,
+                myTypeHandler, isUsedAsStaticType)
+    }
+
+    override fun withTypeHandler(handler: Any?): CollectionType {
+        return CollectionType(myClass, myBindings, mySuperClass, mySuperInterfaces, myElementType, myValueHandler,
+                handler, isUsedAsStaticType)
+    }
+
+    override fun withContentTypeHandler(handler: Any?): CollectionType {
+        return CollectionType(myClass, myBindings, mySuperClass, mySuperInterfaces,
+                myElementType.withTypeHandler(handler), myValueHandler, myTypeHandler, isUsedAsStaticType)
+    }
+
+    override fun withValueHandler(handler: Any?): CollectionType {
+        return CollectionType(myClass, myBindings, mySuperClass, mySuperInterfaces, myElementType, handler,
+                myTypeHandler, isUsedAsStaticType)
+    }
+
+    override fun withContentValueHandler(handler: Any?): CollectionType {
+        return CollectionType(myClass, myBindings, mySuperClass, mySuperInterfaces,
+                myElementType.withValueHandler(handler), myValueHandler, myTypeHandler, isUsedAsStaticType)
+    }
+
+    override fun withStaticTyping(): CollectionType {
+        if (isUsedAsStaticType) {
+            return this
+        }
+
+        return CollectionType(myClass, myBindings, mySuperClass, mySuperInterfaces, myElementType.withStaticTyping(),
+                myValueHandler, myTypeHandler, true)
+    }
+
+    override fun refine(raw: KClass<*>, bindings: TypeBindings, superClass: KotlinType,
+            superInterfaces: Array<KotlinType>): KotlinType {
+        return CollectionType(raw, bindings, superClass, superInterfaces, myElementType, myValueHandler, myTypeHandler,
+                isUsedAsStaticType)
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Standard methods
+     *******************************************************************************************************************
+     */
+
+    override fun toString(): String {
+        return "[collection type; class ${myClass.qualifiedName}, contains $myElementType]"
+    }
+
+    companion object {
+
+        fun construct(collectionType: KClass<*>, bindings: TypeBindings?, superClass: KotlinType?,
+                interfaces: Array<KotlinType>?, elementType: KotlinType): CollectionType {
+            return CollectionType(collectionType, bindings, superClass, interfaces, elementType, null, null, false)
+        }
+
+    }
+
 }
