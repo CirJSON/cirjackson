bf6bcf9be564be2a3ae8ce0b1c44a95492be5128
Vincent Girard <vinccool96@gmail.com>
2025-08-21 11:06:51 -0400

    Added SubtypeResolver

--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/cirjsontype/SubtypeResolver.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/cirjsontype/SubtypeResolver.kt
@@ -1,8 +1,15 @@
 package org.cirjson.cirjackson.databind.cirjsontype
 
 import org.cirjson.cirjackson.core.util.Snapshottable
+import org.cirjson.cirjackson.databind.KotlinType
+import org.cirjson.cirjackson.databind.configuration.MapperConfig
+import org.cirjson.cirjackson.databind.introspection.AnnotatedClass
+import org.cirjson.cirjackson.databind.introspection.AnnotatedMember
 import kotlin.reflect.KClass
 
+/**
+ * Helper object used for handling registration on resolving of super-types to subtypes.
+ */
 abstract class SubtypeResolver : Snapshottable<SubtypeResolver> {
 
     /*
@@ -11,9 +18,11 @@ abstract class SubtypeResolver : Snapshottable<SubtypeResolver> {
      *******************************************************************************************************************
      */
 
-    override fun snapshot(): SubtypeResolver {
-        TODO("Not yet implemented")
-    }
+    /**
+     * Method that has to create a new instance that contains same registration information as this instance, but is not
+     * linked to this instance.
+     */
+    abstract override fun snapshot(): SubtypeResolver
 
     /*
      *******************************************************************************************************************
@@ -21,10 +30,68 @@ abstract class SubtypeResolver : Snapshottable<SubtypeResolver> {
      *******************************************************************************************************************
      */
 
+    /**
+     * Method for registering specified subtypes (possibly including type names); for type entries without name,
+     * nonqualified class name as used as name (unless overridden by annotation).
+     */
     abstract fun registerSubtypes(vararg subtypes: NamedType): SubtypeResolver
 
+    /**
+     * Method for registering specified subtypes (possibly including type names); for type entries without name,
+     * nonqualified class name as used as name (unless overridden by annotation).
+     */
     abstract fun registerSubtypes(vararg subtypes: KClass<*>): SubtypeResolver
 
+    /**
+     * Method for registering specified subtypes (possibly including type names); for type entries without name,
+     * nonqualified class name as used as name (unless overridden by annotation).
+     */
     abstract fun registerSubtypes(subtypes: Collection<KClass<*>>): SubtypeResolver
+
+    /*
+     *******************************************************************************************************************
+     * Subtype resolution
+     *******************************************************************************************************************
+     */
+
+    /**
+     * Method for finding out all reachable subtypes for a property specified by given element (method or field), such
+     * that access is by type, typically needed for serialization (converting from type to type name).
+     *
+     * @param baseType Effective property base type to use; may differ from actual type of property; for structured
+     * types it is content (value) type and NOT structured type.
+     */
+    abstract fun collectAndResolveSubtypesByClass(config: MapperConfig<*>, property: AnnotatedMember?,
+            baseType: KotlinType?): Collection<NamedType>
+
+    /**
+     * Method for finding out all reachable subtypes for given type, such that access is by type, typically needed for
+     * serialization (converting from type to type name).
+     *
+     * @param baseType Effective property base type to use; may differ from actual type of property; for structured
+     * types it is content (value) type and NOT structured type.
+     */
+    abstract fun collectAndResolveSubtypesByClass(config: MapperConfig<*>,
+            baseType: AnnotatedClass): Collection<NamedType>
+
+    /**
+     * Method for finding out all reachable subtypes for a property specified by given element (method or field), such
+     * that access is by type id, typically needed for deserialization (converting from type id to type).
+     *
+     * @param baseType Effective property base type to use; may differ from actual type of property; for structured
+     * types it is content (value) type and NOT structured type.
+     */
+    abstract fun collectAndResolveSubtypesByTypeId(config: MapperConfig<*>, property: AnnotatedMember?,
+            baseType: KotlinType): Collection<NamedType>
+
+    /**
+     * Method for finding out all reachable subtypes for given type, such that access is by type id, typically needed
+     * for deserialization (converting from type id to type).
+     *
+     * @param baseType Effective property base type to use; may differ from actual type of property; for structured
+     * types it is content (value) type and NOT structured type.
+     */
+    abstract fun collectAndResolveSubtypesByTypeId(config: MapperConfig<*>,
+            baseType: AnnotatedClass): Collection<NamedType>
 
 }
--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/cirjsontype/implementation/StandardSubtypeResolver.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/cirjsontype/implementation/StandardSubtypeResolver.kt
@@ -1,12 +1,20 @@
 package org.cirjson.cirjackson.databind.cirjsontype.implementation
 
+import org.cirjson.cirjackson.databind.KotlinType
 import org.cirjson.cirjackson.databind.cirjsontype.NamedType
 import org.cirjson.cirjackson.databind.cirjsontype.SubtypeResolver
+import org.cirjson.cirjackson.databind.configuration.MapperConfig
+import org.cirjson.cirjackson.databind.introspection.AnnotatedClass
+import org.cirjson.cirjackson.databind.introspection.AnnotatedMember
 import kotlin.reflect.KClass
 
 open class StandardSubtypeResolver : SubtypeResolver {
 
     constructor() : super() {
+    }
+
+    override fun snapshot(): SubtypeResolver {
+        TODO("Not yet implemented")
     }
 
     /*
@@ -24,6 +32,32 @@ open class StandardSubtypeResolver : SubtypeResolver {
     }
 
     override fun registerSubtypes(subtypes: Collection<KClass<*>>): StandardSubtypeResolver {
+        TODO("Not yet implemented")
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Resolution by class (serialization)
+     *******************************************************************************************************************
+     */
+
+    override fun collectAndResolveSubtypesByClass(config: MapperConfig<*>, property: AnnotatedMember?,
+            baseType: KotlinType?): Collection<NamedType> {
+        TODO("Not yet implemented")
+    }
+
+    override fun collectAndResolveSubtypesByClass(config: MapperConfig<*>,
+            baseType: AnnotatedClass): Collection<NamedType> {
+        TODO("Not yet implemented")
+    }
+
+    override fun collectAndResolveSubtypesByTypeId(config: MapperConfig<*>, property: AnnotatedMember?,
+            baseType: KotlinType): Collection<NamedType> {
+        TODO("Not yet implemented")
+    }
+
+    override fun collectAndResolveSubtypesByTypeId(config: MapperConfig<*>,
+            baseType: AnnotatedClass): Collection<NamedType> {
         TODO("Not yet implemented")
     }
 
