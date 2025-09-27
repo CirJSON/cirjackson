709aeb9a8461bdf77eef47889ce150232df354ea
Vincent Girard <vinccool96@gmail.com>
2025-09-27 13:37:43 -0400

    Start AnnotatedClassResolver

--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/introspection/AnnotatedClassResolver.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/introspection/AnnotatedClassResolver.kt
@@ -1,11 +1,73 @@
 package org.cirjson.cirjackson.databind.introspection
 
+import org.cirjson.cirjackson.databind.AnnotationIntrospector
+import org.cirjson.cirjackson.databind.KotlinType
 import org.cirjson.cirjackson.databind.configuration.MapperConfig
+import org.cirjson.cirjackson.databind.type.TypeBindings
+import org.cirjson.cirjackson.databind.util.isJdkClass
 import kotlin.reflect.KClass
 
 open class AnnotatedClassResolver {
 
+    private val myConfig: MapperConfig<*>
+
+    private val myIntrospector: AnnotationIntrospector?
+
+    private val myMixInResolver: MixInResolver?
+
+    private val myBindings: TypeBindings
+
+    private val myType: KotlinType?
+
+    private val myClass: KClass<*>
+
+    private val myPrimaryMixin: KClass<*>?
+
+    private val myCollectAnnotations: Boolean
+
+    /*
+     *******************************************************************************************************************
+     * Lifecycle
+     *******************************************************************************************************************
+     */
+
+    internal constructor(config: MapperConfig<*>, type: KotlinType, resolver: MixInResolver?) {
+        myConfig = config
+        myType = type
+        myClass = type.rawClass
+        myMixInResolver = resolver
+        myBindings = type.bindings
+        myIntrospector = config.takeIf { it.isAnnotationProcessingEnabled }?.annotationIntrospector
+        myPrimaryMixin = resolver?.findMixInClassFor(myClass)
+
+        myCollectAnnotations = myIntrospector != null && (myClass.isJdkClass || !myType.isContainerType)
+    }
+
+    internal constructor(config: MapperConfig<*>, clazz: KClass<*>, resolver: MixInResolver?) {
+        myConfig = config
+        myType = null
+        myClass = clazz
+        myMixInResolver = resolver
+        myBindings = TypeBindings.EMPTY
+        myIntrospector = config.takeIf { it.isAnnotationProcessingEnabled }?.annotationIntrospector
+        myPrimaryMixin = resolver?.findMixInClassFor(myClass)
+
+        myCollectAnnotations = myIntrospector != null
+    }
+
     companion object {
+
+        private val NO_ANNOTATIONS = emptyArray<Annotation>()
+
+        private val EMPTY_ANNOTATIONS = AnnotationCollector.EMPTY_ANNOTATIONS
+
+        private val CLASS_OBJECT = Any::class
+
+        private val CLASS_ENUM = Enum::class
+
+        private val CLASS_LIST = List::class
+
+        private val CLASS_MAP = Map::class
 
         /*
          ***************************************************************************************************************
@@ -13,7 +75,7 @@ open class AnnotatedClassResolver {
          ***************************************************************************************************************
          */
 
-        fun resolveWithoutSuperTypes(config: MapperConfig<*>, forType: KClass<*>): AnnotatedClass {
+        fun resolveWithoutSuperTypes(config: MapperConfig<*>?, forType: KClass<*>): AnnotatedClass {
             TODO("Not yet implemented")
         }
 
