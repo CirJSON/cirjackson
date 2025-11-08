10153dce3c6af56faf3b174a5fcd1f3674bec56f
Vincent Girard <vinccool96@gmail.com>
2025-11-08 12:05:56 -0500

    Added ConcreteBeanPropertyBase

--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/introspection/ConcreteBeanPropertyBase.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/introspection/ConcreteBeanPropertyBase.kt
@@ -8,33 +8,76 @@ import org.cirjson.cirjackson.databind.PropertyName
 import org.cirjson.cirjackson.databind.configuration.MapperConfig
 import kotlin.reflect.KClass
 
+/**
+ * Intermediate [BeanProperty] class shared by concrete readable- and writable property implementations for sharing
+ * common functionality.
+ */
 abstract class ConcreteBeanPropertyBase protected constructor(metadata: PropertyMetadata?) : BeanProperty {
+
+    /**
+     * Additional information about property
+     */
+    protected val myMetadata = metadata ?: PropertyMetadata.STANDARD_REQUIRED_OR_OPTIONAL
+
+    protected var myAliases: List<PropertyName>? = null
 
     constructor(source: ConcreteBeanPropertyBase) : this(source.metadata)
 
     override val isRequired: Boolean
-        get() = TODO("Not yet implemented")
+        get() = myMetadata.isRequired
 
     override val metadata: PropertyMetadata
-        get() = TODO("Not yet implemented")
+        get() = myMetadata
 
     override val isVirtual: Boolean
-        get() = TODO("Not yet implemented")
+        get() = false
 
     override fun findFormatOverrides(config: MapperConfig<*>): CirJsonFormat.Value? {
-        TODO("Not yet implemented")
+        val introspector = config.annotationIntrospector ?: return null
+        val member = member ?: return null
+        return introspector.findFormat(config, member)
     }
 
     override fun findPropertyFormat(config: MapperConfig<*>, baseType: KClass<*>): CirJsonFormat.Value {
-        TODO("Not yet implemented")
+        val v1 = config.getDefaultPropertyFormat(baseType)
+        val v2 = findFormatOverrides(config) ?: return v1
+        return v1.withOverrides(v2)
     }
 
     override fun findPropertyInclusion(config: MapperConfig<*>, baseType: KClass<*>): CirJsonInclude.Value? {
-        TODO("Not yet implemented")
+        val introspector = config.annotationIntrospector
+        val member = member ?: return config.getDefaultPropertyInclusion(baseType)
+        val v0 = config.getDefaultInclusion(baseType, member.rawType)
+
+        introspector ?: return v0
+
+        val v = introspector.findPropertyInclusion(config, member)
+        return v0?.withOverrides(v) ?: v
     }
 
     override fun findAliases(config: MapperConfig<*>): List<PropertyName> {
-        TODO("Not yet implemented")
+        var aliases = myAliases
+
+        if (aliases != null) {
+            return aliases
+        }
+
+        val introspector = config.annotationIntrospector
+
+        if (introspector != null) {
+            val member = member
+
+            if (member != null) {
+                aliases = introspector.findPropertyAliases(config, member)
+            }
+        }
+
+        if (aliases == null) {
+            aliases = emptyList()
+        }
+
+        myAliases = aliases
+        return aliases
     }
 
 }
