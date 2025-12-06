6cee7a00abae5c2dcd4af78039249280dac1cd10
Vincent Girard <vinccool96@gmail.com>
2025-12-06 11:04:53 -0500

    Fix constructors

--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/PropertyMetadata.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/PropertyMetadata.kt
@@ -3,13 +3,21 @@ package org.cirjson.cirjackson.databind
 import org.cirjson.cirjackson.annotations.Nulls
 import org.cirjson.cirjackson.databind.introspection.AnnotatedMember
 
-open class PropertyMetadata protected constructor(val required: Boolean?, val description: String?, val index: Int?,
-        defaultValue: String?, val mergeInfo: MergeInfo?, val valueNulls: Nulls?, val contentNulls: Nulls?) {
+open class PropertyMetadata protected constructor(protected val myRequired: Boolean?,
+        protected val myDescription: String?, protected val myIndex: Int?, defaultValue: String?,
+        protected val myMergeInfo: MergeInfo?, protected val myValueNulls: Nulls?,
+        protected val myContentNulls: Nulls?) {
+
+    /*
+     *******************************************************************************************************************
+     * Accessors
+     *******************************************************************************************************************
+     */
 
     val isRequired: Boolean
         get() = TODO("Not yet implemented")
 
-    class MergeInfo(val getter: AnnotatedMember, val fromDefaults: Boolean) {
+    class MergeInfo private constructor(val getter: AnnotatedMember, val fromDefaults: Boolean) {
     }
 
     companion object {
