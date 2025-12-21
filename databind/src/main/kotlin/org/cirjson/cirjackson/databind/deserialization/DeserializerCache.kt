ccb92b5672cb6d5d35367606974b15d86129b99b
Vincent Girard <vinccool96@gmail.com>
2025-12-21 11:59:31 -0500

    Not snapshottable

--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/deserialization/DeserializerCache.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/deserialization/DeserializerCache.kt
@@ -1,16 +1,16 @@
 package org.cirjson.cirjackson.databind.deserialization
 
-import org.cirjson.cirjackson.core.util.Snapshottable
 import org.cirjson.cirjackson.databind.KotlinType
 import org.cirjson.cirjackson.databind.ValueDeserializer
 import org.cirjson.cirjackson.databind.util.LookupCache
 
-class DeserializerCache(private val myCachedDeserializers: LookupCache<KotlinType, ValueDeserializer<Any>>) :
-        Snapshottable<DeserializerCache> {
+class DeserializerCache(private val myCachedDeserializers: LookupCache<KotlinType, ValueDeserializer<Any>>) {
 
-    override fun snapshot(): DeserializerCache {
-        TODO("Not yet implemented")
-    }
+    /*
+     *******************************************************************************************************************
+     * Construction
+     *******************************************************************************************************************
+     */
 
     companion object {
 
