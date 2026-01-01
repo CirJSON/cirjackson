4185a1898ee4115d6907cd570b6246503806c645
Vincent Girard <vinccool96@gmail.com>
2026-01-01 10:38:11 -0500

    Added InternalNodeSerializer and NodeCursor

--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/node/InternalNodeSerializer.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/node/InternalNodeSerializer.kt
@@ -1,6 +1,14 @@
 package org.cirjson.cirjackson.databind.node
 
+import org.cirjson.cirjackson.core.CirJacksonException
+import org.cirjson.cirjackson.core.CirJsonGenerator
+import org.cirjson.cirjackson.databind.CirJacksonSerializable
+import org.cirjson.cirjackson.databind.CirJsonNode
+import org.cirjson.cirjackson.databind.SerializerProvider
 import org.cirjson.cirjackson.databind.cirjson.CirJsonMapper
+import org.cirjson.cirjackson.databind.cirjsontype.TypeSerializer
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * Helper object used to support non-recursive serialization for use by [BaseCirJsonNode.toString] (and related) method.
@@ -13,12 +21,150 @@ object InternalNodeSerializer {
 
     private val PRETTY_WRITER = MAPPER.writerWithDefaultPrettyPrinter()
 
+    private fun wrapper(root: BaseCirJsonNode): CirJacksonSerializable {
+        return WrapperForSerializer(root)
+    }
+
     fun toString(root: BaseCirJsonNode): String {
-        TODO("Not yet implemented")
+        return COMPACT_WRITER.writeValueAsString(wrapper(root))
     }
 
     fun toPrettyString(root: BaseCirJsonNode): String {
-        TODO("Not yet implemented")
+        return PRETTY_WRITER.writeValueAsString(wrapper(root))
+    }
+
+    /**
+     * Intermediate serializer we need to implement non-recursive serialization of [BaseCirJsonNode]
+     */
+    private class WrapperForSerializer(private val myRoot: BaseCirJsonNode) : CirJacksonSerializable.Base() {
+
+        private var myContext: SerializerProvider? = null
+
+        @Throws(CirJacksonException::class)
+        override fun serialize(generator: CirJsonGenerator, context: SerializerProvider) {
+            myContext = context
+            serializeNonRecursive(generator, myRoot)
+        }
+
+        @Throws(CirJacksonException::class)
+        override fun serializeWithType(generator: CirJsonGenerator, context: SerializerProvider,
+                typeSerializer: TypeSerializer) {
+            serialize(generator, context)
+        }
+
+        @Throws(CirJacksonException::class)
+        private fun serializeNonRecursive(generator: CirJsonGenerator, node: CirJsonNode) {
+            when (node) {
+                is ObjectNode -> {
+                    generator.writeStartObject(node, node.size)
+                    serializeNonRecursive(generator, IteratorStack(), node.fields())
+                }
+
+                is ArrayNode -> {
+                    generator.writeStartArray(node, node.size)
+                    serializeNonRecursive(generator, IteratorStack(), node.elements())
+                }
+
+                else -> {
+                    node.serialize(generator, myContext!!)
+                }
+            }
+        }
+
+        @Throws(CirJacksonException::class)
+        @Suppress("UNCHECKED_CAST")
+        private fun serializeNonRecursive(generator: CirJsonGenerator, stack: IteratorStack,
+                rootIterator: Iterator<*>) {
+            var currentIterator = rootIterator
+
+            while (true) {
+                while (currentIterator.hasNext()) {
+                    val element = currentIterator.next()!!
+
+                    val value = if (element is Map.Entry<*, *>) {
+                        val entry = element as Map.Entry<String, CirJsonNode>
+                        generator.writeName(entry.key)
+                        entry.value
+                    } else {
+                        element as CirJsonNode
+                    }
+
+                    when (value) {
+                        is ObjectNode -> {
+                            stack.push(currentIterator)
+                            currentIterator = value.fields()
+                            generator.writeStartObject(value, value.size)
+                        }
+
+                        is ArrayNode -> {
+                            stack.push(currentIterator)
+                            currentIterator = value.elements()
+                            generator.writeStartArray(value, value.size)
+                        }
+
+                        is POJONode -> {
+                            try {
+                                value.serialize(generator, myContext!!)
+                            } catch (e: CirJacksonException) {
+                                generator.writeString("[ERROR: (${e::class.qualifiedName}) ${e.message}]")
+                            }
+                        }
+
+                        else -> {
+                            value.serialize(generator, myContext!!)
+                        }
+                    }
+                }
+
+                if (generator.streamWriteContext.isInArray) {
+                    generator.writeEndArray()
+                } else {
+                    generator.writeEndObject()
+                }
+
+                currentIterator = stack.popOrNull() ?: return
+            }
+        }
+
+    }
+
+    /**
+     * Optimized variant similar in functionality to (a subset of) [ArrayDeque]; used to hold enclosing Array/Object
+     * nodes during recursion-as-iteration.
+     */
+    private class IteratorStack {
+
+        private var myStack: Array<Iterator<*>?>? = null
+
+        private var myTop = 0
+
+        private var myEnd = 0
+
+        fun push(iterator: Iterator<*>) {
+            if (myTop < myEnd) {
+                myStack!![myTop++] = iterator
+                return
+            }
+
+            if (myStack == null) {
+                myEnd = 10
+                myStack = arrayOfNulls(myEnd)
+            } else {
+                myEnd += min(4000, max(20, myEnd shr 1))
+                myStack = myStack!!.copyOf(myEnd)
+            }
+
+            myStack!![myTop++] = iterator
+        }
+
+        fun popOrNull(): Iterator<*>? {
+            if (myTop == 0) {
+                return null
+            }
+
+            return myStack!![--myTop]
+        }
+
     }
 
 }
---
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/node/NodeCursor.kt
@@ -0,0 +1,196 @@
+package org.cirjson.cirjackson.databind.node
+
+import org.cirjson.cirjackson.core.CirJsonToken
+import org.cirjson.cirjackson.core.TokenStreamContext
+import org.cirjson.cirjackson.databind.CirJsonNode
+
+/**
+ * Helper class used by [TreeTraversingParser] to keep track of current location within traversed CirJSON tree.
+ *
+ * @property myParent Parent cursor of this cursor, if any; `null` for root cursors.
+ */
+abstract class NodeCursor(contextType: Int, protected val myParent: NodeCursor?) : TokenStreamContext() {
+
+    /**
+     * Current field name
+     */
+    protected var myCurrentName: String? = null
+
+    protected var myCurrentValue: Any? = null
+
+    init {
+        myType = contextType
+        myIndex = -1
+    }
+
+    /*
+     *******************************************************************************************************************
+     * CirJsonStreamContext implementation
+     *******************************************************************************************************************
+     */
+
+    final override val parent: NodeCursor?
+        get() = myParent
+
+    final override val currentName: String?
+        get() = myCurrentName
+
+    open fun overrideCurrentName(name: String?) {
+        myCurrentName = name
+    }
+
+    override fun currentValue(): Any? {
+        return myCurrentValue
+    }
+
+    override fun assignCurrentValue(value: Any?) {
+        myCurrentValue = value
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Extended API
+     *******************************************************************************************************************
+     */
+
+    abstract fun nextToken(): CirJsonToken?
+
+    abstract fun currentNode(): CirJsonNode?
+
+    abstract fun startArray(): NodeCursor
+
+    abstract fun startObject(): NodeCursor
+
+    /**
+     * Method called to create a new context for iterating all contents of the current structured value (CirJSON array
+     * or object)
+     */
+    fun iterateChildren(): NodeCursor {
+        val node = currentNode() ?: throw IllegalStateException("No current node")
+
+        return if (node.isArray) {
+            ArrayCursor(node, this)
+        } else if (node.isObject) {
+            ObjectCursor(node, this)
+        } else {
+            throw IllegalStateException("Current node of type ${node::class.qualifiedName}")
+        }
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Concrete implementations
+     *******************************************************************************************************************
+     */
+
+    /**
+     * Context for all root-level value nodes (including Arrays and Objects): only context for scalar values.
+     */
+    protected class RootCursor(node: CirJsonNode, parent: NodeCursor?) : NodeCursor(TYPE_ROOT, parent) {
+
+        private var myNode: CirJsonNode? = node
+
+        private var myDone = false
+
+        override fun overrideCurrentName(name: String?) {
+            // No-op
+        }
+
+        override fun nextToken(): CirJsonToken? {
+            if (myDone) {
+                myNode = null
+                return null
+            }
+
+            myIndex++
+            myDone = true
+            return myNode!!.asToken()
+        }
+
+        override fun currentNode(): CirJsonNode? {
+            return myNode?.takeIf { myDone }
+        }
+
+        override fun startArray(): NodeCursor {
+            return ArrayCursor(myNode!!, this)
+        }
+
+        override fun startObject(): NodeCursor {
+            return ObjectCursor(myNode!!, this)
+        }
+
+    }
+
+    protected class ArrayCursor(node: CirJsonNode, parent: NodeCursor?) : NodeCursor(TYPE_ARRAY, parent) {
+
+        private val myContents = node.elements()
+
+        private var myCurrentElement: CirJsonNode? = null
+
+        override fun nextToken(): CirJsonToken {
+            if (!myContents.hasNext()) {
+                myCurrentElement = null
+                return CirJsonToken.END_ARRAY
+            }
+
+            myIndex++
+            myCurrentElement = myContents.next()
+            return myCurrentElement!!.asToken()
+        }
+
+        override fun currentNode(): CirJsonNode? {
+            return myCurrentElement
+        }
+
+        override fun startArray(): NodeCursor {
+            return ArrayCursor(myCurrentElement!!, this)
+        }
+
+        override fun startObject(): NodeCursor {
+            return ObjectCursor(myCurrentElement!!, this)
+        }
+
+    }
+
+    protected class ObjectCursor(node: CirJsonNode, parent: NodeCursor?) : NodeCursor(TYPE_OBJECT, parent) {
+
+        private val myContents = node.fields()
+
+        private var myCurrentEntry: Map.Entry<String, CirJsonNode>? = null
+
+        private var myNeedEntry = true
+
+        override fun nextToken(): CirJsonToken {
+            if (!myNeedEntry) {
+                myNeedEntry = true
+                return myCurrentEntry!!.value.asToken()
+            }
+
+            if (!myContents.hasNext()) {
+                myCurrentName = null
+                myCurrentEntry = null
+                return CirJsonToken.END_OBJECT
+            }
+
+            myIndex++
+            myNeedEntry = false
+            myCurrentEntry = myContents.next()
+            myCurrentName = myCurrentEntry?.key
+            return CirJsonToken.PROPERTY_NAME
+        }
+
+        override fun currentNode(): CirJsonNode? {
+            return myCurrentEntry?.value
+        }
+
+        override fun startArray(): NodeCursor {
+            return ArrayCursor(currentNode()!!, this)
+        }
+
+        override fun startObject(): NodeCursor {
+            return ObjectCursor(currentNode()!!, this)
+        }
+
+    }
+
+}
