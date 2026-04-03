872a487d42e49a697ac65cc83f71d08bb113ded5
Vincent Girard <vinccool96@gmail.com>
2026-04-03 13:49:17 -0400

    Forgotten base override

--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/serialization/implementation/AttributePropertyWriter.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/serialization/implementation/AttributePropertyWriter.kt
@@ -1,7 +1,9 @@
 package org.cirjson.cirjackson.databind.serialization.implementation
 
 import org.cirjson.cirjackson.annotations.CirJsonInclude
+import org.cirjson.cirjackson.core.CirJsonGenerator
 import org.cirjson.cirjackson.databind.KotlinType
+import org.cirjson.cirjackson.databind.SerializerProvider
 import org.cirjson.cirjackson.databind.configuration.MapperConfig
 import org.cirjson.cirjackson.databind.introspection.AnnotatedClass
 import org.cirjson.cirjackson.databind.introspection.BeanPropertyDefinition
@@ -21,6 +23,16 @@ open class AttributePropertyWriter(protected val myAttributeName: String, proper
 
     override fun withConfig(config: MapperConfig<*>, declaringClass: AnnotatedClass,
             propertyDefinition: BeanPropertyDefinition, type: KotlinType): VirtualBeanPropertyWriter {
+        TODO("Not yet implemented")
+    }
+
+    /*
+     *******************************************************************************************************************
+     * Overrides for actual serialization, value access
+     *******************************************************************************************************************
+     */
+
+    override fun value(bean: Any, generator: CirJsonGenerator, provider: SerializerProvider): Any? {
         TODO("Not yet implemented")
     }
 
