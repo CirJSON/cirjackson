a6c1558d3625390abd59fbc4a918c727c8f7b592
Vincent Girard <vinccool96@gmail.com>
2026-05-17 11:07:13 -0400

    Fix constructor

--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/deserialization/implementation/ObjectIdReader.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/deserialization/implementation/ObjectIdReader.kt
@@ -8,6 +8,6 @@ import org.cirjson.cirjackson.databind.ValueDeserializer
 import org.cirjson.cirjackson.databind.deserialization.SettableBeanProperty
 
 open class ObjectIdReader protected constructor(protected val myIdType: KotlinType, val propertyName: PropertyName,
-        val generator: ObjectIdGenerator<*>, val resolver: ObjectIdResolver,
-        protected val myDeserializer: ValueDeserializer<Any>, val idProperty: SettableBeanProperty?) {
+        val generator: ObjectIdGenerator<*>, val resolver: ObjectIdResolver, deserializer: ValueDeserializer<*>,
+        val idProperty: SettableBeanProperty?) {
 }
