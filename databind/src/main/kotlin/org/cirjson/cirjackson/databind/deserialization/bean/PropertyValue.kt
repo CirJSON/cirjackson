a1b41622ca7a534d84fcb8974a6a640f82149dfb
Vincent Girard <vinccool96@gmail.com>
2026-06-06 10:36:33 -0400

    Fix constructor

--- databind/src/main/kotlin/org/cirjson/cirjackson/databind/deserialization/bean/PropertyValue.kt
+++ databind/src/main/kotlin/org/cirjson/cirjackson/databind/deserialization/bean/PropertyValue.kt
@@ -2,7 +2,7 @@ package org.cirjson.cirjackson.databind.deserialization.bean
 
 import org.cirjson.cirjackson.core.CirJacksonException
 
-abstract class PropertyValue protected constructor(val next: PropertyValue?, val value: Any) {
+abstract class PropertyValue protected constructor(val next: PropertyValue?, value: Any) {
 
     @Throws(CirJacksonException::class)
     abstract fun assign(bean: Any)
